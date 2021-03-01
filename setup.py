5ebe52032f8bd82740583287b1d2809814016b3c
Constantin Pape <c.pape@gmx.net>
2021-03-01 15:37:57 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import runpy
+from setuptools import setup, find_packages
+
+__version__ = runpy.run_path("torch_em/__version__.py")["__version__"]
+
+
+# NOTE requirements are not all available via pip, you need to use conda,
+# see 'environment_gpu.yaml' / 'environment_cpu.yaml'
+requires = [
+    "torch",
+    "h5py"
+]
+
+
+setup(
+    name="torch_em",
+    packages=find_packages(exclude=["test"]),
+    version=__version__,
+    author="Constantin Pape",
+    install_requires=requires,
+    url="https://github.com/constantinpape/torch-em",
+    license="MIT"
+)
