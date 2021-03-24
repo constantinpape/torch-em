02421e618f7195acde54a80df4cb774b66315057
Constantin Pape <constantin.pape@embl.de>
2021-03-24 21:01:48 +0100

    Add EMDefectAugmentation WIP

--- torch_em/transform/__init__.py
+++ torch_em/transform/__init__.py
@@ -1,3 +1,3 @@
 from .augmentation import get_augmentations
 from .label import AffinityTransform, BoundaryTransform, labels_to_binary
-from .raw import get_raw_transform
+from .raw import get_raw_transform, EMDefectAugmentation
