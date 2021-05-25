1796178085f7dd2a92d5fd12fd323f2916bb7359
fynnbe <thefynnbe@gmail.com>
2021-05-25 20:16:09 +0200

    test Tile with unittests

--- test/transform/test_generic.py
+++ test/transform/test_generic.py
@@ -1,28 +1,36 @@
 import numpy
 import torch
-import pytest
 
 from torch_em.transform import Tile
 
 
-@pytest.mark.parametrize("ndim,reps", [(1, (4, 2)), (2, (4, 2)), (3, (4, 2))])
-def test_tile(ndim, reps):
-    tile_aug = Tile(reps, match_shape_exactly=len(reps) == ndim)
-    test_shape = [2, 3, 4][:ndim]
-    data = numpy.random.random(test_shape)
+from unittest import TestCase
 
-    x = torch.tensor(data)
 
-    expected = numpy.tile(x.numpy(), reps)
-    if len(reps) == ndim:
-        expected_torch = x.repeat(*reps)
-        assert expected.shape == expected_torch.shape
+class TestTile(TestCase):
+    def test_tile(self):
+        for ndim, reps in [(1, (4, 2)), (2, (4, 2)), (3, (4, 2))]:
+            with self.subTest():
+                self._test_tile_impl(ndim, reps)
 
-    actual = tile_aug(x)
+    @staticmethod
+    def _test_tile_impl(ndim, reps):
+        tile_aug = Tile(reps, match_shape_exactly=len(reps) == ndim)
+        test_shape = [2, 3, 4][:ndim]
+        data = numpy.random.random(test_shape)
 
-    assert actual.shape == expected.shape
+        x = torch.tensor(data)
 
-    a = numpy.array(data)
+        expected = numpy.tile(x.numpy(), reps)
+        if len(reps) == ndim:
+            expected_torch = x.repeat(*reps)
+            assert expected.shape == expected_torch.shape
 
-    actual = tile_aug(a)
-    assert actual.shape == expected.shape
+        actual = tile_aug(x)
+
+        assert actual.shape == expected.shape
+
+        a = numpy.array(data)
+
+        actual = tile_aug(a)
+        assert actual.shape == expected.shape
