12021d3bf111ff67fa912ba56c0b32bc49545a82
Constantin Pape <c.pape@gmx.net>
2021-06-11 00:11:39 +0200

    Update modelzoo export functionality

--- torch_em/util/__init__.py
+++ torch_em/util/__init__.py
@@ -1,5 +1,7 @@
 from .image import load_image, supports_memmap
-from .modelzoo import import_bioimageio_model, export_biomageio_model
+from .modelzoo import (export_biomageio_model,
+                       get_default_citations,
+                       import_bioimageio_model)
 from .training import parser_helper
 from .util import (ensure_array, ensure_spatial_array,
                    ensure_tensor, ensure_tensor_with_channels,
