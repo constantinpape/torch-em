a1e00cd87b08d989512a6b120f407308fc74fad6
Constantin Pape <constantin.pape@embl.de>
2021-06-21 11:12:54 +0200

    Add dsb dataset and update dsb training

--- torch_em/data/datasets/__init__.py
+++ torch_em/data/datasets/__init__.py
@@ -1,4 +1,5 @@
 from .cremi import get_cremi_loader
+from .dsb import get_dsb_loader
 from .isbi2012 import get_isbi_loader
 from .platynereis import (get_platynereis_cell_loader,
                           get_platynereis_nuclei_loader)
