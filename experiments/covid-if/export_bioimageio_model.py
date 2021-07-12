451f66f5e9d3fe89273e0696609b77bc1f87e6c5
Constantin Pape <constantin.pape@embl.de>
2021-07-12 18:23:11 +0200

    Implement covid-if dataset and affinity training; update some bioimage-io exporters

--- experiments/covid-if/export_bioimageio_model.py
+++ experiments/covid-if/export_bioimageio_model.py
@@ -1,28 +1,13 @@
 import argparse
 import os
 
-import numpy as np
-from elf.io import open_file
+import h5py
 from torch_em.util import (convert_to_onnx, convert_to_pytorch_script,
                            export_biomageio_model, get_default_citations)
 
 
-def _load_data(input_, ndim):
-    with open_file(input_, 'r') as f:
-        ds = f['volumes/raw'] if 'volumes/raw' in f else f['raw']
-        shape = ds.shape
-        if ndim == 2:
-            s0, s1 = shape[0] - 1, shape[0]
-            bb = np.s_[s0:s1, :, :]
-        else:
-            assert False, "3d not supported yet"
-        raw = ds[bb]
-    return raw
-
-
-def _get_name(is_aff, ndim):
-    name = "ISBI2012"
-    name += "-2D" if ndim == 2 else "-3D"
+def _get_name(is_aff):
+    name = "Covid-IF-Cells"
     if is_aff:
         name += "-AffinityModel"
     else:
@@ -30,61 +15,65 @@ def _get_name(is_aff, ndim):
     return name
 
 
-def _get_doc(is_aff_model, ndim):
+def _get_doc(is_aff_model):
+    ndim = 2
     if is_aff_model:
         doc = f"""
 ## {ndim}D U-Net for Affinity Prediction
 
-This model was trained on the data of the ISBI2012 neuron segmentation challenge.
-It predicts affinity maps that can be processed with the mutex watershed to obtain
-an instance segmentation.
+This model was trained on HTM data from a Covid-IF antibody test.
+It predicts affinity maps and foreground probabilities for cell segmentation.
+The affinities can be processed with the mutex watershed to obtain an instance segmentation.
         """
     else:
         doc = f"""
 ## {ndim}D U-Net for Boundary Prediction
 
-This model was trained on the data of the ISBI2012 neuron segmentation challenge.
-It predicts boundary maps that can be processed with multicut segmentation to obtain
-an instance segmentation.
-        """
+This model was trained on HTM data from a Covid-IF antibody test.
+It predicts boundary maps and foreground probabilities for cell segmentation.
+The boundaries can be processed with multicut segmentation to obtain an instance segmentation.
+Or they can be combined with seeds obtained via a (DAPI) nucleus segmentation for a watershed segmentation."""
     return doc
 
 
-# need to wait on the spec pr to fix this.
 # TODO write offsets and other mws params into the config if this is a affinity model
-def export_to_bioimageio(checkpoint, input_, output, affs_to_bd, additional_formats):
+def export_to_bioimageio(checkpoint, output, input_, affs_to_bd, additional_formats):
 
     ckpt_name = os.path.split(checkpoint)[1]
-
-    ndim = 3 if '3d' in ckpt_name else 2
-    input_data = _load_data(input_, ndim)
+    if input_ is None:
+        input_data = None
+    else:
+        with h5py.File(input_, 'r') as f:
+            input_data = f['raw/serum_IgG/s0'][:]
 
     is_aff_model = 'affinity' in ckpt_name
     if is_aff_model and affs_to_bd:
-        postprocessing = f'affinities_to_boundaries{ndim}d'
+        postprocessing = 'affinities_with_foreground_to_boundaries2d'
     else:
         postprocessing = None
 
     if is_aff_model and affs_to_bd:
         is_aff_model = False
-    name = _get_name(is_aff_model, ndim)
-    tags = ["u-net", "neuron-segmentation", "segmentation", "volume-em", "isbi2012-challenge"]
+    name = _get_name(is_aff_model)
+    tags = ["u-net", "cell-segmentation", "htm", "high-throughput-microscopt", "segmentation", "cells",
+            "covid-antibody-test", "covid-19", "sars-cov-2", "immunofluorescence"]
     tags += ["boundary-prediction"] if is_aff_model else ["affinity-prediction"]
 
     # eventually we should refactor the citation logic
     cite = get_default_citations()
-    cite["data"] = "doi.org/10.3389/fnana.2015.00142"
-    if ndim == 2:
-        cite["architecture"] = "https://link.springer.com/chapter/10.1007/978-3-319-24574-4_28"
-    else:
-        cite["architecture"] = "https://link.springer.com/chapter/10.1007/978-3-319-46723-8_49"
+    covid_if_pub = "https://doi.org/10.1002/bies.202000257"
+    cite["data"] = covid_if_pub
+    cite["architecture"] = "https://link.springer.com/chapter/10.1007/978-3-319-46723-8_49"
     if is_aff_model:
         cite["segmentation algorithm"] = "10.1109/TPAMI.2020.2980827"
+    else:
+        cite["segmentation algorithm"] = covid_if_pub
 
-    doc = _get_doc(is_aff_model, ndim)
+    doc = _get_doc(is_aff_model)
 
     export_biomageio_model(
-        checkpoint, output, input_data,
+        checkpoint, output,
+        input_data=input_data,
         name=name,
         authors=[{"name": "Constantin Pape; @constantinpape"}],
         tags=tags,
@@ -94,6 +83,7 @@ def export_to_bioimageio(checkpoint, input_, output, affs_to_bd, additional_form
         cite=cite,
         model_postprocessing=postprocessing,
         input_optional_parameters=False,
+        # need custom deepimagej fields if we have torchscript export
         for_deepimagej="torchscript" in additional_formats
     )
 
@@ -106,12 +96,13 @@ def export_to_bioimageio(checkpoint, input_, output, affs_to_bd, additional_form
 
 
 if __name__ == '__main__':
+    # TODO refactor this into a parser helper
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--checkpoint', required=True)
-    parser.add_argument('-i', '--input', required=True)
     parser.add_argument('-o', '--output', required=True)
+    parser.add_argument('-i', '--input', required=True)
     parser.add_argument('-a', '--affs_to_bd', default=0, type=int)
     parser.add_argument('-f', '--additional_formats', type=str, nargs="+")
     args = parser.parse_args()
-    export_to_bioimageio(args.checkpoint, args.input, args.output,
+    export_to_bioimageio(args.checkpoint, args.output, args.input,
                          bool(args.affs_to_bd), args.additional_formats)
