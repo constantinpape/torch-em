3a2330966e49e95ce8bd08518f042c0c8805f5ef
Constantin Pape <c.pape@gmx.net>
2021-07-12 11:38:41 +0200

    Add more model exports WIP

---
+++ torch_em/data/datasets/covd_if.py
@@ -0,0 +1,13 @@
+import os
+from shutil import move
+
+import torch_em
+from .util import download_source, unzip, update_kwargs
+
+
+def _download_covid_if():
+    pass
+
+
+def get_covid_if_loader():
+    pass
