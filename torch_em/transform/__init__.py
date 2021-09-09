13e2064e37c187d15abe4bf7bc6e1cc5b0520514
Constantin Pape <constantin.pape@embl.de>
2021-09-09 14:38:14 +0200

    Implement EM defect augmentations

--- torch_em/transform/__init__.py
+++ torch_em/transform/__init__.py
@@ -1,4 +1,5 @@
 from .augmentation import get_augmentations
+from .defect import EMDefectAugmentation, get_artifact_source
 from .generic import Tile
 from .label import AffinityTransform, BoundaryTransform, labels_to_binary
-from .raw import get_raw_transform, EMDefectAugmentation
+from .raw import get_raw_transform
