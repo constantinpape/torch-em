5ce2c7f857315657f39bb1e456bcea119f6a4f6e
Constantin Pape <c.pape@gmx.net>
2021-09-14 14:37:11 +0200

    Add livecell dataset

--- torch_em/util/debug.py
+++ torch_em/util/debug.py
@@ -2,14 +2,18 @@ import napari
 from .util import ensure_array
 
 
-def check_loader(loader, n_samples):
+def check_loader(loader, n_samples, instance_labels=False):
     for ii, (x, y) in enumerate(loader):
         if ii >= n_samples:
             break
 
         x = ensure_array(x).squeeze(0)
         y = ensure_array(y).squeeze(0)
-        with napari.gui_qt():
-            v = napari.Viewer()
-            v.add_image(x)
+
+        v = napari.Viewer()
+        v.add_image(x)
+        if instance_labels:
+            v.add_labels(y.astype("uint32"))
+        else:
             v.add_image(y)
+        napari.run()
