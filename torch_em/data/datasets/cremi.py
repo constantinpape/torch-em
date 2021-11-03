cfda02198af4e1a68e061ac2f27a316c4f0ce081
fynnbe <thefynnbe@gmail.com>
2021-11-03 12:13:57 +0100

    add shuffle as explicit loader kwarg

--- torch_em/data/datasets/cremi.py
+++ torch_em/data/datasets/cremi.py
@@ -43,6 +43,7 @@ def get_cremi_loader(
     },
     batch_size=1,
     num_workers: int = 0,
+    shuffle: bool = False,
     loader_kwargs=None,
     **dataset_kwargs,
 ):
@@ -60,7 +61,9 @@ def get_cremi_loader(
         defect_augmentation_kwargs=defect_augmentation_kwargs,
         **dataset_kwargs,
     )
-    return torch_em.get_data_loader(ds, batch_size=batch_size, num_workers=num_workers, **(loader_kwargs or {}))
+    return torch_em.get_data_loader(
+        ds, batch_size=batch_size, num_workers=num_workers, shuffle=shuffle, **(loader_kwargs or {})
+    )
 
 
 def get_cremi_dataset(
