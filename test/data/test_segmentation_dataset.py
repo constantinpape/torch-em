c36fab07e13f0049801f510ace33aba9b6d22c57
Constantin Pape <constantin.pape@embl.de>
2021-11-24 18:21:23 +0100

    Support training with channels

--- test/data/test_segmentation_dataset.py
+++ test/data/test_segmentation_dataset.py
@@ -1,18 +1,27 @@
 import os
 import unittest
+
+import h5py
 import numpy as np
 from torch_em.util.test import create_segmentation_test_data
 
 
 class TestSegmentationDataset(unittest.TestCase):
-    path = './data.h5'
-    raw_key = 'raw'
-    label_key = 'labels'
+    path = "./data.h5"
+    raw_key = "raw"
+    channel_key = "raw_with_channels"
+    label_key = "labels"
     shape = (128,) * 3
 
     def setUp(self):
+        chunks = (32,) * 3
         create_segmentation_test_data(self.path, self.raw_key, self.label_key,
-                                      shape=self.shape, chunks=(32,) * 3)
+                                      shape=self.shape, chunks=chunks)
+        with h5py.File(self.path, "a") as f:
+            shape = (3,) + self.shape
+            chunks = (1,) + chunks
+            data_with_channels = np.random.rand(*shape)
+            f.create_dataset(self.channel_key, data=data_with_channels, chunks=chunks)
 
     def tearDown(self):
         os.remove(self.path)
@@ -52,6 +61,20 @@ class TestSegmentationDataset(unittest.TestCase):
             self.assertEqual(x.shape, expected_shape)
             self.assertEqual(y.shape, expected_shape)
 
+    def test_with_channels(self):
+        from torch_em.data import SegmentationDataset
+        patch_shape = (32, 32, 32)
+        ds = SegmentationDataset(self.path, self.channel_key,
+                                 self.path, self.label_key,
+                                 patch_shape=patch_shape, ndim=3)
+        self.assertEqual(ds._ndim, 3)
+        expected_raw_shape = (3,) + patch_shape
+        expected_label_shape = (1,) + patch_shape
+        for i in range(10):
+            x, y = ds[i]
+            self.assertEqual(x.shape, expected_raw_shape)
+            self.assertEqual(y.shape, expected_label_shape)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
