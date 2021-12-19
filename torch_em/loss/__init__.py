59f2c37af701b4f122f2344cd5260863e7b2ce81
Constantin Pape <c.pape@gmx.net>
2021-12-19 22:12:13 +0100

    Add affinity embedding side loss

--- torch_em/loss/__init__.py
+++ torch_em/loss/__init__.py
@@ -1,3 +1,5 @@
+from .affinity_side_loss import AffinitySideLoss
+from .combined_loss import CombinedLoss
 from .contrastive import ContrastiveLoss
 from .dice import DiceLoss, dice_score
 from .wrapper import ApplyAndRemoveMask, LossWrapper
