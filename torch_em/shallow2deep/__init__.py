2d483c4d5dc59c54163dd3ec1814a3f7ae40fccb
Constantin Pape <constantin.pape@embl.de>
2022-07-04 08:26:46 +0200

    Add function to check s2d predictions WIP

--- torch_em/shallow2deep/__init__.py
+++ torch_em/shallow2deep/__init__.py
@@ -1,4 +1,6 @@
-from .prepare_shallow2deep import prepare_shallow2deep, prepare_shallow2deep_advanced
+from .prepare_shallow2deep import (prepare_shallow2deep,
+                                   prepare_shallow2deep_advanced,
+                                   visualize_pretrained_rfs)
 from .pseudolabel_training import get_pseudolabel_loader
 from .shallow2deep_dataset import get_shallow2deep_loader
 from .transform import BoundaryTransform, ForegroundTransform
