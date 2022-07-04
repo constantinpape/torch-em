8893e90f025afe0eebdb0d3af1d1402e8aea0059
Constantin Pape <constantin.pape@embl.de>
2022-07-05 00:08:09 +0200

    Fix issues in s2d em-mito training

--- torch_em/shallow2deep/__init__.py
+++ torch_em/shallow2deep/__init__.py
@@ -1,6 +1,5 @@
-from .prepare_shallow2deep import (prepare_shallow2deep,
-                                   prepare_shallow2deep_advanced,
-                                   visualize_pretrained_rfs)
+from .prepare_shallow2deep import prepare_shallow2deep, prepare_shallow2deep_advanced
 from .pseudolabel_training import get_pseudolabel_loader
 from .shallow2deep_dataset import get_shallow2deep_loader
+from .shallow2deep_eval import visualize_pretrained_rfs
 from .transform import BoundaryTransform, ForegroundTransform
