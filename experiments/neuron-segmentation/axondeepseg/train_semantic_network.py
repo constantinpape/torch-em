6f10bf29d165d622412b3923bde4a6316a3abdff
Constantin Pape <constantin.pape@embl.de>
2022-07-14 14:04:21 +0200

    Update axondeepseg training

--- experiments/neuron-segmentation/axondeepseg/train_semantic_network.py
+++ experiments/neuron-segmentation/axondeepseg/train_semantic_network.py
@@ -3,9 +3,15 @@ from torch_em.model import UNet2d
 from torch_em.data.datasets import get_axondeepseg_loader
 
 
+def normalize_datasets(datasets):
+    datasets = list(sorted(args.datasets))
+    return datasets
+
+
 def get_loader(args, patch_shape, split):
     data_fraction = 0.85 if split == "train" else 0.15
-    return get_axondeepseg_loader(args.input, "sem", download=True, one_hot_encoding=[0, 1, 2],
+    datasets = normalize_datasets(args.datasets)
+    return get_axondeepseg_loader(args.input, datasets, download=True, one_hot_encoding=[0, 1, 2],
                                   data_fraction=data_fraction,
                                   patch_shape=patch_shape,
                                   shuffle=True, ndim=2,
@@ -20,12 +26,13 @@ def train_semantic_network(args):
     model = UNet2d(in_channels=1, out_channels=n_out, final_activation="Sigmoid")
 
     # shape of input patches used for training
-    patch_shape = [1024, 1024]
+    patch_shape = [576, 576]
 
     train_loader = get_loader(args, patch_shape, "train")
     val_loader = get_loader(args, patch_shape, "val")
 
-    name = "dice-axon-model"
+    datasets = normalize_datasets(args.datasets)
+    name = f"dice-axon-model-{'_'.join(datasets)}"
     trainer = torch_em.default_segmentation_trainer(
         name=name,
         model=model,
@@ -41,7 +48,7 @@ def train_semantic_network(args):
 
 def check(args, n_images=2):
     from torch_em.util.debug import check_loader
-    patch_shape = [1024, 1024]
+    patch_shape = [768, 768]
 
     print("Check train loader")
     loader = get_loader(args, patch_shape, "train")
@@ -52,9 +59,9 @@ def check(args, n_images=2):
     check_loader(loader, n_images)
 
 
-# TODO support sem and tem
 if __name__ == "__main__":
-    parser = torch_em.util.parser_helper()
+    parser = torch_em.util.parser_helper(default_batch_size=8)
+    parser.add_argument("-d", "--datasets", nargs="+", default=["sem", "tem"])
     args = parser.parse_args()
     if args.check:
         check(args)
