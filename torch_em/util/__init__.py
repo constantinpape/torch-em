b99b89401df243da81e203c39f59ec91aca35e94
Constantin Pape <c.pape@gmx.net>
2022-07-18 21:52:59 +0200

    Add modelzoo config functionality WIP

--- torch_em/util/__init__.py
+++ torch_em/util/__init__.py
@@ -6,6 +6,8 @@ from .modelzoo import (add_weight_formats,
                        export_parser_helper,
                        get_default_citations,
                        import_bioimageio_model)
+from .modelzoo_configs import (get_mws_config,
+                               get_shallow2deep_config)
 from .reporting import get_training_summary
 from .training import parser_helper
 from .util import (ensure_array, ensure_spatial_array,
