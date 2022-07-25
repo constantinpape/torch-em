2149dfc2303a1cff97fbb2f8bb774126a1188759
Constantin Pape <c.pape@gmx.net>
2022-07-25 23:34:26 +0200

    Add tests for augmentations WIP

---
+++ test/transform/test_augmentations.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestElasticDeformations(unittest.TestCase):
+    def test_elastic_deformation_2d(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
