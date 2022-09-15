cf7e34bf68f24db9c4f01bcf435a2c7b386604dd
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2022-09-15 11:20:40 +0200

    Make bioimageio import optional

--- torch_em/util/__init__.py
+++ torch_em/util/__init__.py
@@ -1,15 +1,17 @@
 from .image import load_image, supports_memmap
-from .modelzoo import (add_weight_formats,
-                       convert_to_onnx,
-                       convert_to_torchscript,
-                       export_bioimageio_model,
-                       export_parser_helper,
-                       get_default_citations,
-                       import_bioimageio_model)
-from .modelzoo_configs import (get_mws_config,
-                               get_shallow2deep_config)
 from .reporting import get_training_summary
 from .training import parser_helper
 from .util import (ensure_array, ensure_spatial_array,
                    ensure_tensor, ensure_tensor_with_channels,
                    get_constructor_arguments, get_trainer)
+
+# NOTE: we don't import the modelzoo convenience functions here.
+# In order to avoid importing bioimageio.core (which is quite massive) when importing torch_em
+# and to enable running torch_em without bioimageio.core
+# from .modelzoo import (add_weight_formats,
+#                        convert_to_onnx,
+#                        convert_to_torchscript,
+#                        export_bioimageio_model,
+#                        export_parser_helper,
+#                        get_default_citations,
+#                        import_bioimageio_model)
