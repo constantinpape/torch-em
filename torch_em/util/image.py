284089db6a710ab1f484dc8f81d6429b9dca1984
Grzegorz Bokota <bokota+github@gmail.com>
2022-09-20 08:53:58 +0000

    use tifffile.imread, add simple tests

--- torch_em/util/image.py
+++ torch_em/util/image.py
@@ -1,7 +1,7 @@
 # TODO this should be partially refactored into elf.io before the next elf release
 # and then be used in image_stack_wrapper as welll
 import os
-import imageio
+import imageio.v2 as imageio
 
 try:
     import tifffile
@@ -27,6 +27,8 @@ def supports_memmap(image_path):
 def load_image(image_path):
     if supports_memmap(image_path):
         return tifffile.memmap(image_path, mode='r')
+    elif tifffile is not None and os.path.splitext(image_path)[1].lower() in {".tiff", ".tif"}:
+        return tifffile.imread(image_path)
     else:
         # TODO handle multi-channel images
         return imageio.imread(image_path)
