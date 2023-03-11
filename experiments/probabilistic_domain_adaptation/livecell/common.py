f9f0dff69328e53ef6c0318285e8189791f4663e
Constantin Pape <c.pape@gmx.net>
2023-03-11 22:48:39 +0100

    Livecell training updates

--- experiments/probabilistic_domain_adaptation/livecell/common.py
+++ experiments/probabilistic_domain_adaptation/livecell/common.py
@@ -1,6 +1,34 @@
 import argparse
 
+import torch_em
+from torch_em.data.datasets import get_livecell_loader
+from torchvision import transforms
+
 CELL_TYPES = ["A172", "BT474", "BV2", "Huh7", "MCF7", "SHSY5Y", "SkBr3", "SKOV3"]
+
+
+#
+# The augmentations we use for the LiveCELL experiments:
+# - weak augmenations: blurring and additive gaussian noise
+# - strong augmentations: TODO
+#
+
+
+def weak_augmentations(p=0.25):
+    norm = torch_em.transform.raw.standardize
+    aug = transforms.Compose([
+        norm,
+        transforms.RandomApply([torch_em.transform.raw.GaussianBlur()], p=p),
+        transforms.RandomApply([torch_em.transform.raw.AdditiveGaussianNoise(
+            scale=(0, 0.15), clip_kwargs=False)], p=p
+        ),
+    ])
+    return torch_em.transform.raw.get_raw_transform(normalizer=norm, augmentation1=aug)
+
+
+#
+# Other utility functions: parser, loaders
+#
 
 
 def get_parser(default_batch_size=8, default_iterations=int(1e5)):
@@ -12,3 +40,17 @@ def get_parser(default_batch_size=8, default_iterations=int(1e5)):
     parser.add_argument("-s", "--save_root")
     parser.add_argument("-c", "--cell_types", nargs="+", default=CELL_TYPES)
     return parser
+
+
+def get_supervised_loader(args, split, cell_type):
+    patch_shape = (512, 512)
+    loader = get_livecell_loader(
+        args.input, patch_shape, split,
+        download=True, binary=True, batch_size=args.batch_size,
+        cell_types=[cell_type], num_workers=8, shuffle=True,
+    )
+    return loader
+
+
+def get_unsupervised_loader(args, split, cell_type):
+    pass
--- experiments/probabilistic_domain_adaptation/livecell/unet_source.py
+++ experiments/probabilistic_domain_adaptation/livecell/unet_source.py
@@ -1,23 +1,12 @@
 import torch_em
 from torch_em.model import UNet2d
-from torch_em.data.datasets import get_livecell_loader
-from common import get_parser
-
-
-def _get_loader(args, split, cell_type):
-    patch_shape = (512, 512)
-    loader = get_livecell_loader(
-        args.input, patch_shape, split,
-        download=True, binary=True, batch_size=args.batch_size,
-        cell_types=[cell_type], num_workers=8, shuffle=True,
-    )
-    return loader
+from common import get_parser, get_supervised_loader
 
 
 def _train_cell_type(args, cell_type):
     model = UNet2d(in_channels=1, out_channels=1, initial_features=64, final_activation="Sigmoid")
-    train_loader = _get_loader(args, "train", cell_type)
-    val_loader = _get_loader(args, "train", cell_type)
+    train_loader = get_supervised_loader(args, "train", cell_type)
+    val_loader = get_supervised_loader(args, "val", cell_type)
     name = f"unet_source/{cell_type}"
     trainer = torch_em.default_segmentation_trainer(
         name=name,
@@ -45,7 +34,7 @@ def check_loader(args, n_images=5):
     print("The cell types", cell_types, "were selected.")
     print("Checking the loader for the first cell type", cell_types[0])
 
-    loader = _get_loader(args)
+    loader = get_supervised_loader(args)
     check_loader(loader, n_images)
 
 
