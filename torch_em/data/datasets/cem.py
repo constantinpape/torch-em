08ad776aca5b2504a198f41a19165e327b3af56b
Constantin Pape <c.pape@gmx.net>
2023-03-19 17:43:23 +0100

    Implemet CEM data loaders WIP

---
+++ torch_em/data/datasets/cem.py
@@ -0,0 +1,23 @@
+# Data loaders for the CEM datasets:
+# - CEM-MitoLab: annotated data for training mitochondria segmentation models
+#   - https://www.ebi.ac.uk/empiar/EMPIAR-11037/
+# - CEM-1.5M: unlabeled EM images for pretraining:
+#   - https://www.ebi.ac.uk/empiar/EMPIAR-11035/
+# - CEM-Mito-Benchmark: 7 Benchmark datasets for mitochondria segmentation
+#   - https://www.ebi.ac.uk/empiar/EMPIAR-10982/
+
+
+# TODO
+# - implement download
+def get_cem_mitolab_loader(path):
+    pass
+
+
+# TODO
+def get_cem15m_loader(path):
+    pass
+
+
+# TODO
+def get_cem_mito_benchmark_loader(path):
+    pass
