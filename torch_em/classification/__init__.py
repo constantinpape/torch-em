06f3427ebddeaec18474127691f5226872cb9c4c
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2023-05-21 23:43:07 +0200

    Implement remaining classification functionality

--- torch_em/classification/__init__.py
+++ torch_em/classification/__init__.py
@@ -1,2 +1,4 @@
+from .classification import default_classification_trainer, default_classification_loader
+
 from .classification_logger import ClassificationLogger
 from .classification_trainer import ClassificationTrainer
