dc45eb8efd1858c2f3b7ba4a87fe9a974d15389e
anwai98 <anwai.archit@gmail.com>
2023-09-13 18:12:26 +0200

    Fix and Test different ViT models

--- experiments/vision-transformer/unetr/initialize_with_sam.py
+++ experiments/vision-transformer/unetr/initialize_with_sam.py
@@ -5,7 +5,8 @@ from torch_em.model.unetr import build_unetr_with_sam_initialization
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = build_unetr_with_sam_initialization(
-    checkpoint_path="/scratch/usr/nimanwai/models/segment-anything/checkpoints/sam_vit_b_01ec64.pth")
+    model_type="vit_h",
+    checkpoint_path="/scratch/usr/nimanwai/models/segment-anything/checkpoints/sam_vit_h_4b8939.pth")
 model.to(device)
 
 x = torch.randn(1, 3, 1024, 1024).to(device=device)
