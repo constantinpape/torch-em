485d713d97c1265bf91f2a959524d54482cf9ed7
Anwai Archit <anwai.archit@gmail.com>
2023-10-05 21:07:28 +0200

    Refactor download and h5 conversion scripts

--- torch_em/data/datasets/pannuke.py
+++ torch_em/data/datasets/pannuke.py
@@ -22,73 +22,34 @@ URLS = {
 CHECKSUM = None
 
 
-def _download_pannuke_dataset(path, download):
+def _download_pannuke_dataset(path, download, folds):
     os.makedirs(path, exist_ok=True)
 
-    is_fold_1 = os.path.exists(os.path.join(path, "pannuke_fold_1.h5"))
-    is_fold_2 = os.path.exists(os.path.join(path, "pannuke_fold_2.h5"))
-    is_fold_3 = os.path.exists(os.path.join(path, "pannuke_fold_3.h5"))
-    if is_fold_1 and is_fold_2 and is_fold_3 is True:
-        return
-
     checksum = CHECKSUM
-    util.download_source(os.path.join(path, "fold_1.zip"), URLS["fold_1"], download, checksum)
-    util.download_source(os.path.join(path, "fold_2.zip"), URLS["fold_2"], download, checksum)
-    util.download_source(os.path.join(path, "fold_3.zip"), URLS["fold_3"], download, checksum)
 
-    print("Unzipping the PanNuke dataset in their respective fold directories...")
-    util.unzip(os.path.join(path, "fold_1.zip"), os.path.join(path, "fold_1"), True)
-    util.unzip(os.path.join(path, "fold_2.zip"), os.path.join(path, "fold_2"), True)
-    util.unzip(os.path.join(path, "fold_3.zip"), os.path.join(path, "fold_3"), True)
+    for tmp_fold in folds:
+        if os.path.exists(os.path.join(path, f"pannuke_{tmp_fold}.h5")):
+            return
 
-    _assort_pannuke_dataset(path, download)
+        util.download_source(os.path.join(path, f"{tmp_fold}.zip"), URLS[f"{tmp_fold}"], download, checksum)
 
-
-def _assort_pannuke_dataset(path):
-    print("Assorting the PanNuke dataset in the expected structure...")
-    for _p in glob(os.path.join(path, "*", "*")):
-        dst = os.path.split(_p)[0]
-        for src_sub_dir in glob(os.path.join(_p, "*")):
-            f_dst = os.path.join(dst, os.path.split(src_sub_dir)[-1])
-            shutil.move(src_sub_dir, f_dst)
-        shutil.rmtree(_p)
+        print(f"Unzipping the PanNuke dataset in {tmp_fold} directories...")
+        util.unzip(os.path.join(path, f"{tmp_fold}.zip"), os.path.join(path, f"{tmp_fold}"), True)
 
 
-def _convert_to_hdf5(path):
-    h5_f1_path = os.path.join(path, "pannuke_fold_1.h5")
-    h5_f2_path = os.path.join(path, "pannuke_fold_2.h5")
-    h5_f3_path = os.path.join(path, "pannuke_fold_3.h5")
+def _convert_to_hdf5(path, folds):
+    for tmp_fold in folds:
+        if os.path.exists(os.path.join(path, f"pannuke_{tmp_fold}.h5")):
+            return
 
-    if os.path.exists(h5_f1_path) and os.path.exists(h5_f2_path) and os.path.exists(h5_f3_path) is True:
-        return
+        print(f"Converting the {tmp_fold} into h5 file format...")
+        tmp_name = tmp_fold.split("_")[0] + tmp_fold.split("_")[1]  # name of a particular sub-directory (per fold)
+        with h5py.File(os.path.join(path, f"pannuke_{tmp_fold}.h5"), "w") as f:
+            img_path = glob(os.path.join(path, tmp_fold, "*", "images", tmp_name, "images.npy"))[0]
+            gt_path = glob(os.path.join(path, tmp_fold, "*", "masks", tmp_name, "masks.npy"))[0]
 
-    print("Converting the folds into h5 file format...")
-    # fold_1
-    with h5py.File(h5_f1_path, "w") as f:
-        f.create_dataset(
-            "images",
-            data=np.load(os.path.join(path, "fold_1", "images", "fold1", "images.npy")).transpose(3, 0, 1, 2))
-        f.create_dataset(
-            "masks",
-            data=np.load(os.path.join(path, "fold_1", "masks", "fold1", "masks.npy")).transpose(3, 0, 1, 2))
-
-    # fold_2
-    with h5py.File(h5_f2_path, "w") as f:
-        f.create_dataset(
-            "images",
-            data=np.load(os.path.join(path, "fold_2", "images", "fold2", "images.npy")).transpose(3, 0, 1, 2))
-        f.create_dataset(
-            "masks",
-            data=np.load(os.path.join(path, "fold_2", "masks", "fold2", "masks.npy")).transpose(3, 0, 1, 2))
-
-    # fold_3
-    with h5py.File(h5_f3_path, "w") as f:
-        f.create_dataset(
-            "images",
-            data=np.load(os.path.join(path, "fold_3", "images", "fold3", "images.npy")).transpose(3, 0, 1, 2))
-        f.create_dataset(
-            "masks",
-            data=np.load(os.path.join(path, "fold_3", "masks", "fold3", "masks.npy")).transpose(3, 0, 1, 2))
+            f.create_dataset("images", data=np.load(img_path).transpose(3, 0, 1, 2))
+            f.create_dataset("masks", data=np.load(gt_path).transpose(3, 0, 1, 2))
 
     dir_to_rm = glob(os.path.join(path, "*[!.h5]"))
     for tmp_dir in dir_to_rm:
@@ -108,8 +69,8 @@ def get_pannuke_dataset(
     if rois is not None:
         assert isinstance(rois, dict)
 
-    _download_pannuke_dataset(path, download)
-    _convert_to_hdf5(path)
+    _download_pannuke_dataset(path, download, folds)
+    _convert_to_hdf5(path, folds)
 
     data_paths = [os.path.join(path, f"pannuke_{f_idx}.h5") for f_idx in folds]
     data_rois = [rois.get(f_idx, np.s_[:, :, :]) for f_idx in folds]
