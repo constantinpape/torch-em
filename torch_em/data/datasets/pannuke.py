9640e85da89117508805f6e7cd9c0c03ceb8854a
Anwai Archit <anwai.archit@gmail.com>
2023-10-05 14:40:46 +0200

    Add pannuke dataset and dataloader

--- torch_em/data/datasets/pannuke.py
+++ torch_em/data/datasets/pannuke.py
@@ -4,6 +4,7 @@ import shutil
 import numpy as np
 from glob import glob
 
+import torch_em
 from torch_em.data.datasets import util
 
 
@@ -22,9 +23,9 @@ CHECKSUM = None
 def _download_pannuke_dataset(path, download):
     os.makedirs(path, exist_ok=True)
 
-    is_fold_1 = os.path.exists(os.path.join(path, "fold_1"))
-    is_fold_2 = os.path.exists(os.path.join(path, "fold_2"))
-    is_fold_3 = os.path.exists(os.path.join(path, "fold_3"))
+    is_fold_1 = os.path.exists(os.path.join(path, "pannuke_fold_1.h5"))
+    is_fold_2 = os.path.exists(os.path.join(path, "pannuke_fold_2.h5"))
+    is_fold_3 = os.path.exists(os.path.join(path, "pannuke_fold_3.h5"))
     if is_fold_1 and is_fold_2 and is_fold_3 is True:
         return
 
@@ -41,15 +42,14 @@ def _download_pannuke_dataset(path, download):
     _assort_pannuke_dataset(path, download)
 
 
-def _assort_pannuke_dataset(path, download):
-    if download:
-        print("Assorting the PanNuke dataset in the expected structure...")
-        for _p in glob(os.path.join(path, "*", "*")):
-            dst = os.path.split(_p)[0]
-            for src_sub_dir in glob(os.path.join(_p, "*")):
-                f_dst = os.path.join(dst, os.path.split(src_sub_dir)[-1])
-                shutil.move(src_sub_dir, f_dst)
-            shutil.rmtree(_p)
+def _assort_pannuke_dataset(path):
+    print("Assorting the PanNuke dataset in the expected structure...")
+    for _p in glob(os.path.join(path, "*", "*")):
+        dst = os.path.split(_p)[0]
+        for src_sub_dir in glob(os.path.join(_p, "*")):
+            f_dst = os.path.join(dst, os.path.split(src_sub_dir)[-1])
+            shutil.move(src_sub_dir, f_dst)
+        shutil.rmtree(_p)
 
 
 def _convert_to_hdf5(path):
@@ -63,38 +63,106 @@ def _convert_to_hdf5(path):
     print("Converting the folds into h5 file format...")
     # fold_1
     with h5py.File(h5_f1_path, "w") as f:
-        f.create_dataset("images", data=np.load(os.path.join(path, "fold_1", "images", "fold1", "images.npy")))
-        f.create_dataset("masks", data=np.load(os.path.join(path, "fold_1", "masks", "fold1", "masks.npy")))
+        f.create_dataset(
+            "images",
+            data=np.load(os.path.join(path, "fold_1", "images", "fold1", "images.npy")).transpose(3, 0, 1, 2))
+        f.create_dataset(
+            "masks",
+            data=np.load(os.path.join(path, "fold_1", "masks", "fold1", "masks.npy")).transpose(3, 0, 1, 2))
 
     # fold_2
     with h5py.File(h5_f2_path, "w") as f:
-        f.create_dataset("images", data=np.load(os.path.join(path, "fold_2", "images", "fold2", "images.npy")))
-        f.create_dataset("masks", data=np.load(os.path.join(path, "fold_2", "masks", "fold2", "masks.npy")))
+        f.create_dataset(
+            "images",
+            data=np.load(os.path.join(path, "fold_2", "images", "fold2", "images.npy")).transpose(3, 0, 1, 2))
+        f.create_dataset(
+            "masks",
+            data=np.load(os.path.join(path, "fold_2", "masks", "fold2", "masks.npy")).transpose(3, 0, 1, 2))
 
     # fold_3
     with h5py.File(h5_f3_path, "w") as f:
-        f.create_dataset("images", data=np.load(os.path.join(path, "fold_3", "images", "fold3", "images.npy")))
-        f.create_dataset("masks", data=np.load(os.path.join(path, "fold_3", "masks", "fold3", "masks.npy")))
+        f.create_dataset(
+            "images",
+            data=np.load(os.path.join(path, "fold_3", "images", "fold3", "images.npy")).transpose(3, 0, 1, 2))
+        f.create_dataset(
+            "masks",
+            data=np.load(os.path.join(path, "fold_3", "masks", "fold3", "masks.npy")).transpose(3, 0, 1, 2))
 
     dir_to_rm = glob(os.path.join(path, "*[!.h5]"))
     for tmp_dir in dir_to_rm:
         shutil.rmtree(tmp_dir)
 
 
-def get_pannuke_dataset(path, download):
+def get_pannuke_dataset(
+        path,
+        patch_shape,
+        folds=("fold_1", "fold_2", "fold_3"),
+        rois={},
+        download=False,
+        with_channels=True,
+        with_label_channels=True,
+        **kwargs
+):
+    if rois is not None:
+        assert isinstance(rois, dict)
+
     _download_pannuke_dataset(path, download)
     _convert_to_hdf5(path)
 
+    data_paths = [os.path.join(path, f"pannuke_{f_idx}.h5") for f_idx in folds]
+    data_rois = [rois.get(f_idx, np.s_[:, :, :]) for f_idx in folds]
 
-def get_pannuke_loader(path, download):
-    get_pannuke_dataset(path, download)
+    raw_key = "images"
+    label_key = "masks"
+
+    return torch_em.default_segmentation_dataset(
+        data_paths, raw_key, data_paths, label_key, patch_shape, rois=data_rois,
+        with_channels=with_channels, with_label_channels=with_label_channels, **kwargs
+    )
+
+
+def get_pannuke_loader(
+        path,
+        patch_shape,
+        batch_size,
+        folds=("fold_1", "fold_2", "fold_3"),
+        download=False,
+        rois={},
+        **kwargs
+):
+    """TODO
+    """
+    dataset_kwargs, loader_kwargs = util.split_kwargs(torch_em.default_segmentation_dataset, **kwargs)
+
+    ds = get_pannuke_dataset(
+        path=path,
+        patch_shape=patch_shape,
+        folds=folds,
+        rois=rois,
+        download=download,
+        **dataset_kwargs)
+    return torch_em.get_data_loader(ds, batch_size=batch_size, **loader_kwargs)
+
+
+def label_trafo(labels):
+    breakpoint()
+    return labels
 
 
 def main():
-    path = "./pannuke/"
-    download = True
+    train_loader = get_pannuke_loader(
+        path="./pannuke/",
+        batch_size=2,
+        patch_shape=(1, 256, 256),
+        ndim=2,
+        download=True,
+        label_transform=label_trafo
+    )
 
-    get_pannuke_loader(path, download)
+    for x, y in next(iter(train_loader)):
+        breakpoint()
+
+    breakpoint()
 
 
 if __name__ == "__main__":
