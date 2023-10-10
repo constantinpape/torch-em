bdb82c1f4c3f16b2ecb0764e830c1ac23007b787
anwai98 <anwai.archit@gmail.com>
2023-10-10 23:24:31 +0200

    Update h5 conversion and adding label trafo

--- torch_em/data/datasets/pannuke.py
+++ torch_em/data/datasets/pannuke.py
@@ -18,7 +18,6 @@ URLS = {
 }
 
 
-# TODO
 CHECKSUM = {
     "fold_1": "6e19ad380300e8ce9480f9ab6a14cc91fa4b6a511609b40e3d70bdf9c881ed0b",
     "fold_2": "5bc540cc509f64b5f5a274d6e5a245527dbd3e6d3155d43555115c5d54709b07",
@@ -52,13 +51,88 @@ def _convert_to_hdf5(path, fold):
     gt_paths = glob(os.path.join(path, "**", "masks.npy"), recursive=True)
 
     for img_path, gt_path in zip(img_paths, gt_paths):
+        # original (raw) shape : S x H x W x C -> transposed shape (expected) : C x S x H x W
+        img = np.load(img_path)
+        labels = np.load(gt_path)
+
+        instances = _channels_to_instances(labels)
+        semantic = _channels_to_semantics(labels)
+
+        img = img.transpose(3, 0, 1, 2)
+        labels = labels.transpose(3, 0, 1, 2)
+
+        # img.shape -> (3, 2656, 256, 256) --- img_chunks -> (3, 1, 256, 256)
+        # (same logic as above for labels)
+        img_chunks = (img.shape[0], 1) + img.shape[2:]
+        label_chunks = (labels.shape[0], 1) + labels.shape[2:]
+        other_label_chunks = (1) + labels.shape[2:]  # for instance and semantic labels
+
+        breakpoint()
+
         with h5py.File(os.path.join(path, f"pannuke_{fold}.h5"), "w") as f:
-            f.create_dataset("images", data=np.load(img_path).transpose(3, 0, 1, 2))
-            f.create_dataset("masks", data=np.load(gt_path).transpose(3, 0, 1, 2))
+            f.create_dataset("images", data=img, compression="gzip", chunks=img_chunks)
+            f.create_dataset("labels/masks", data=labels, compression="gzip", chunks=label_chunks)
+            f.create_dataset("labels/instances", data=instances, compression="gzip", chunks=other_label_chunks)
+            f.create_dataset("labels/semantic", data=semantic, compression="gzip", chunks=other_label_chunks)
 
     dir_to_rm = glob(os.path.join(path, "*[!.h5]"))
     for tmp_dir in dir_to_rm:
         shutil.rmtree(tmp_dir)
+
+
+def _channels_to_instances(labels):
+    """Converting the ground-truth of 6 (instance) channels into 1 label with instances from all channels
+    channel info -
+    (0: Neoplastic cells, 1: Inflammatory, 2: Connective/Soft tissue cells, 3: Dead Cells, 4: Epithelial, 6: Background)
+
+    Returns:
+        - instance labels of dimensions -> (C x H x W)
+    """
+    labels = labels.transpose(0, 3, 1, 2)  # to access with the shape S x 6 x H x W
+    list_of_instances = []
+
+    for label_slice in labels:  # access the slices (each with 6 channels of H x W labels)
+        segmentation = np.zeros(labels.shape[2:])
+        max_ids = []
+        for label_channel in label_slice[:-1]:  # access the channels
+            # the 'start_label' takes care of where to start allocating the instance ids from
+            this_labels, max_id, _ = vigra.analysis.relabelConsecutive(
+                label_channel.astype("uint64"),
+                start_label=max_ids[-1] + 1 if len(max_ids) > 0 else 1)
+
+            # some trailing channels might not have labels, hence appending only for elements with RoIs
+            if max_id > 0:
+                max_ids.append(max_id)
+
+            segmentation[this_labels > 0] = this_labels[this_labels > 0]
+
+        list_of_instances.append(segmentation)
+
+    f_segmentation = np.stack(list_of_instances)
+
+    return f_segmentation
+
+
+def _channels_to_semantics(labels):
+    """Converting the ground-truth of 6 (instance) channels  into semantic labels, ollowing below the id info as:
+    (1 -> Neoplastic cells, 2 -> Inflammatory, 3 -> Connective/Soft tissue cells,
+    4 -> Dead Cells, 5 -> Epithelial, 0 -> Background)
+
+    Returns:
+        - semantic labels of dimensions -> (C x H x W)
+    """
+    labels = labels.transpose(0, 3, 1, 2)
+    list_of_semantic = []
+
+    for label_slice in labels:
+        segmentation = np.zeros(labels.shape[2:])
+        for i, label_channel in enumerate(label_slice[:-1]):
+            segmentation[label_channel > 0] = i + 1
+        list_of_semantic.append(segmentation)
+
+    f_segmentation = np.stack(list_of_semantic)
+
+    return f_segmentation
 
 
 def get_pannuke_dataset(
@@ -111,36 +185,13 @@ def get_pannuke_loader(
     return torch_em.get_data_loader(ds, batch_size=batch_size, **loader_kwargs)
 
 
-def label_trafo(labels):
-    """Converting the ground-truth of 6 (instance) channels into 1 label with instances from all channels
-    channel info -
-    (0: Neoplastic cells, 1: Inflammatory, 2: Connective/Soft tissue cells, 3: Dead Cells, 4: Epithelial, 6: Background)
-    """
-    segmentation = np.zeros(labels.shape[1:])
-    max_ids = []
-    for label_channel in labels[:-1]:
-        # the 'start_label' takes care of where to start allocating the instance ids from
-        this_labels, max_id, _ = vigra.analysis.relabelConsecutive(
-            label_channel.astype("uint64"),
-            start_label=max_ids[-1] + 1 if len(max_ids) > 0 else 1)
-
-        # some trailing channels might not have labels, hence appending only for elements with RoIs
-        if max_id > 0:
-            max_ids.append(max_id)
-
-        segmentation[this_labels > 0] = this_labels[this_labels > 0]
-
-    return segmentation
-
-
 def main():
     train_loader = get_pannuke_loader(
         path="./pannuke/",
         batch_size=2,
         patch_shape=(1, 256, 256),
         ndim=2,
-        download=True,
-        label_transform=label_trafo
+        download=True
     )
     check_loader(train_loader, 8, instance_labels=True, plt=False, rgb=True)
 
