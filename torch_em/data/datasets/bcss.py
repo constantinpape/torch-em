5b680cc32c1f423d4aaba6c8f6e131e2d5634e87
anwai98 <anwai.archit@gmail.com>
2023-10-18 15:30:52 +0200

    Add label transform for semantic instance choice

--- torch_em/data/datasets/bcss.py
+++ torch_em/data/datasets/bcss.py
@@ -1,6 +1,9 @@
 import os
+import numpy as np
 from glob import glob
-from typing import Tuple
+from typing import Tuple, List, Optional
+
+import vigra
 
 import torch
 
@@ -16,6 +19,36 @@ URL = "https://drive.google.com/drive/folders/1zqbdkQF8i5cEmZOGmbdQm-EP8dRYtvss?
 CHECKSUM = None
 
 
+class BCSSLabelTrafo():
+    def __init__(
+            self,
+            label_choices: Optional[List[int]] = None
+    ):
+        """Label choices:
+            0: outside_roi; 1: tumor; 2: stroma; 3: lymphocytic_infiltrate; 4: necrosis_or_debris;
+            5: glandular_secretions; 6: blood; 7: exclude; 8: metaplasia_NOS; 9: fat; 10: plasma_cells;
+            11: other_immune_infiltrate; 12: mucoid_material; 13: normal_acinus_or_duct; 14: lymphatics;
+            15: undetermined; 16: nerve; 17: skin_adnexa; 18: blood_vessel; 19: angioinvasion; 20: dcis; 21: other
+        """
+        self.label_choices = label_choices
+
+    def __call__(
+            self,
+            labels: np.ndarray
+    ) -> np.ndarray:
+        """Returns the transformed labels
+        """
+        if self.label_choices is not None:
+            _segmentation = np.zeros(labels.shape)
+            for uq_label in self.label_choices:
+                _segmentation[labels == uq_label] = labels[labels == uq_label]
+            segmentation, _, _ = vigra.analysis.relabelConsecutive(_segmentation.astype("uint64"))
+        else:
+            segmentation, _, _ = vigra.analysis.relabelConsecutive(labels)
+
+        return segmentation
+
+
 def _download_bcss_dataset(path, download):
     """Current recommendation:
         - download the folder from URL manually
@@ -23,19 +56,13 @@ def _download_bcss_dataset(path, download):
     """
     raise NotImplementedError("Please download the dataset using the drive link / git repo directly")
 
-    # FIXME: current limitation for the installation below:
+    # FIXME: limitation for the installation below:
     #   - only downloads first 50 files - due to `gdown`'s download folder function
     #   - (optional) clone their git repo to download their data
     util.download_source_gdrive(path=path, url=URL, download=download, checksum=CHECKSUM, download_type="folder")
 
 
-def get_bcss_dataset(
-        path: str,
-        patch_shape: Tuple[int, int],
-        download: bool = False,
-        label_dtype: torch.dtype = torch.int64,
-        **kwargs
-):
+def get_bcss_dataset(path, patch_shape, label_choices, download=False, label_dtype=torch.int64, **kwargs):
     """Dataset for breast cancer tissue segmentation in histopathology.
 
     This dataset is from https://bcsegmentation.grand-challenge.org/BCSS/.
@@ -56,22 +83,24 @@ def get_bcss_dataset(
         raise ValueError(
             "Please check the image directory. If downloaded from gdrive, it's named \"rgbs_colorNormalized\", if from github it's named \"images\""
         )
-
     assert len(image_paths) == len(label_paths)
 
+    label_trafo = BCSSLabelTrafo(label_choices=label_choices)
+
     dataset = ImageCollectionDataset(
-        image_paths, label_paths, patch_shape=patch_shape, label_dtype=label_dtype, **kwargs
+        image_paths, label_paths, patch_shape=patch_shape, label_dtype=label_dtype, label_transform=label_trafo,
+        **kwargs
     )
     return dataset
 
 
 def get_bcss_loader(
-        path, patch_shape, batch_size, download=False, label_dtype=torch.int64, **kwargs
+        path, patch_shape, batch_size, label_choices, download=False, label_dtype=torch.int64, **kwargs
 ):
     """Dataloader for breast cancer tissue segmentation in histopathology. See `get_bcss_dataset` for details."""
     ds_kwargs, loader_kwargs = util.split_kwargs(torch_em.default_segmentation_dataset, **kwargs)
     dataset = get_bcss_dataset(
-        path, patch_shape, download=download, label_dtype=label_dtype, **ds_kwargs
+        path, patch_shape, label_choices, download=download, label_dtype=label_dtype, **ds_kwargs
     )
     loader = torch_em.get_data_loader(dataset, batch_size, **loader_kwargs)
     return loader
@@ -82,7 +111,11 @@ def main():
     download = False
 
     loader = get_bcss_loader(
-        path=path, patch_shape=(256, 256), batch_size=2, download=download
+        path=path,
+        patch_shape=(256, 256),
+        batch_size=2,
+        label_choices=None,
+        download=download
     )
 
     print(len(loader))
