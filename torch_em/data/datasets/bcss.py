7cede6e537bdd17e21b29103d72db33b1d7ac24b
anwai98 <anwai.archit@gmail.com>
2023-10-18 14:05:21 +0200

    Raise warnings for bcss download

--- torch_em/data/datasets/bcss.py
+++ torch_em/data/datasets/bcss.py
@@ -16,6 +16,19 @@ URL = "https://drive.google.com/drive/folders/1zqbdkQF8i5cEmZOGmbdQm-EP8dRYtvss?
 CHECKSUM = None
 
 
+def _download_bcss_dataset(path, download):
+    """Current recommendation:
+        - download the folder from URL manually
+        - use the consortium's git repo to download the dataset (https://github.com/PathologyDataScience/BCSS)
+    """
+    raise NotImplementedError("Please download the dataset using the drive link / git repo directly")
+
+    # FIXME: current limitation for the installation below:
+    #   - only downloads first 50 files - due to `gdown`'s download folder function
+    #   - (optional) clone their git repo to download their data
+    util.download_source_gdrive(path=path, url=URL, download=download, checksum=CHECKSUM, download_type="folder")
+
+
 def get_bcss_dataset(
         path: str,
         patch_shape: Tuple[int, int],
@@ -28,15 +41,22 @@ def get_bcss_dataset(
     This dataset is from https://bcsegmentation.grand-challenge.org/BCSS/.
     Please cite this paper (https://doi.org/10.1093/bioinformatics/btz083) if you use this dataset for a publication.
     """
+    if download:
+        _download_bcss_dataset(path, download)
 
-    # FIXME: current limitation for the installation below:
-    #   - only downloads first 50 files - due to `gdown`'s download folder function
-    #   - (optional) clone their git repo to download their data
-    util.download_source_gdrive(path=path, url=URL, download=download, checksum=CHECKSUM, download_type="folder")
+    # when downloading the files from `URL`, the input images are stored under `rgbs_colorNormalized`
+    # when getting the files from the git repo's command line feature, the input images are stored under `images`
+    if os.path.exists(os.path.join(path, "images")):
+        image_paths = sorted(glob(os.path.join(path, "images", "*")))
+        label_paths = sorted(glob(os.path.join(path, "masks", "*")))
+    elif os.path.exists(os.path.join(path, "0_Public-data-Amgad2019_0.25MPP", "rgbs_colorNormalized")):
+        image_paths = sorted(glob(os.path.join(path, "0_Public-data-Amgad2019_0.25MPP", "rgbs_colorNormalized", "*")))
+        label_paths = sorted(glob(os.path.join(path, "0_Public-data-Amgad2019_0.25MPP", "masks", "*")))
+    else:
+        raise ValueError(
+            "Please check the image directory. If downloaded from gdrive, it's named \"rgbs_colorNormalized\", if from github it's named \"images\""
+        )
 
-    image_paths = sorted(glob(os.path.join(path, "*", "rgbs_colorNormalized", "*")))
-    label_paths = sorted(glob(os.path.join(path, "*", "masks", "*")))
-    print(len(image_paths), len(label_paths))
     assert len(image_paths) == len(label_paths)
 
     dataset = ImageCollectionDataset(
@@ -58,8 +78,8 @@ def get_bcss_loader(
 
 
 def main():
-    path = "/scratch/usr/nimanwai/data/test_bcss/"
-    download = True
+    path = "/scratch/usr/nimanwai/data/bcss/"
+    download = False
 
     loader = get_bcss_loader(
         path=path, patch_shape=(256, 256), batch_size=2, download=download
