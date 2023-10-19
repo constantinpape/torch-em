0cf2dda14e98f5166ea8a26da45c6e3e0435bf2f
anwai98 <anwai.archit@gmail.com>
2023-10-19 21:33:02 +0200

    Add organ-level splits for monuseg

--- torch_em/data/datasets/monuseg.py
+++ torch_em/data/datasets/monuseg.py
@@ -2,6 +2,7 @@ import os
 import shutil
 from tqdm import tqdm
 from glob import glob
+from typing import List, Optional
 
 import imageio.v3 as imageio
 
@@ -19,8 +20,22 @@ CHECKSUM = {
     "test": "13e522387ae8b1bcc0530e13ff9c7b4d91ec74959ef6f6e57747368d7ee6f88a"
 }
 
+# here's the description: https://drive.google.com/file/d/1xYyQ31CHFRnvTCTuuHdconlJCMk2SK7Z/view?usp=sharing
+ORGAN_SPLITS = {
+    "breast": ["TCGA-A7-A13E-01Z-00-DX1", "TCGA-A7-A13F-01Z-00-DX1", "TCGA-AR-A1AK-01Z-00-DX1",
+               "TCGA-AR-A1AS-01Z-00-DX1", "TCGA-E2-A1B5-01Z-00-DX1", "TCGA-E2-A14V-01Z-00-DX1"],
+    "kidney": ["TCGA-B0-5711-01Z-00-DX1", "TCGA-HE-7128-01Z-00-DX1", "TCGA-HE-7129-01Z-00-DX1",
+               "TCGA-HE-7130-01Z-00-DX1", "TCGA-B0-5710-01Z-00-DX1", "TCGA-B0-5698-01Z-00-DX1"],
+    "liver": ["TCGA-18-5592-01Z-00-DX1", "TCGA-38-6178-01Z-00-DX1", "TCGA-49-4488-01Z-00-DX1",
+              "TCGA-50-5931-01Z-00-DX1", "TCGA-21-5784-01Z-00-DX1", "TCGA-21-5786-01Z-00-DX1"],
+    "prostate": ["TCGA-G9-6336-01Z-00-DX1", "TCGA-G9-6348-01Z-00-DX1", "TCGA-G9-6356-01Z-00-DX1",
+                 "TCGA-G9-6363-01Z-00-DX1", "TCGA-CH-5767-01Z-00-DX1", "TCGA-G9-6362-01Z-00-DX1"],
+    "bladder": ["TCGA-DK-A2I6-01A-01-TS1", "TCGA-G2-A2EK-01A-02-TSB"],
+    "colon": ["TCGA-AY-A8YK-01A-01-TS1", "TCGA-NH-A8F7-01A-01-TS1"],
+    "stomach": ["TCGA-KB-A93J-01A-01-TS1", "TCGA-RD-A8N9-01A-01-TS1"]
+}
 
-# TODO separate via organ
+
 def _download_monuseg(path, download, split):
     assert split in ["train", "test"], "The split choices in MoNuSeg datset are train/test, please choose from them"
 
@@ -76,32 +91,44 @@ def _process_monuseg(path, split):
 
 
 def get_monuseg_dataset(
-    path, patch_shape, split, download=False, offsets=None, boundaries=False, binary=False, **kwargs
+    path, patch_shape, split, organ_type: Optional[List[str]] = None, download=False,
+    offsets=None, boundaries=False, binary=False, **kwargs
 ):
     """Dataset from https://monuseg.grand-challenge.org/Data/
     """
     _download_monuseg(path, download, split)
 
-    image_path = os.path.join(path, "images", split)
-    label_path = os.path.join(path, "labels", split)
+    image_paths = sorted(glob(os.path.join(path, "images", split, "*")))
+    label_paths = sorted(glob(os.path.join(path, "labels", split, "*")))
+
+    if split == "train" and organ_type is not None:
+        # get all patients for multiple organ selection
+        all_organ_splits = sum([ORGAN_SPLITS[o][:] for o in organ_type], [])
+
+        image_paths = [
+            _path for _path in image_paths if os.path.split(_path)[-1].split(".")[0] in all_organ_splits
+        ]
+        label_paths = [
+            _path for _path in label_paths if os.path.split(_path)[-1].split(".")[0] in all_organ_splits
+        ]
 
     kwargs, _ = util.add_instance_label_transform(
         kwargs, add_binary_target=True, binary=binary, boundaries=boundaries, offsets=offsets
     )
     return torch_em.default_segmentation_dataset(
-        image_path, "*.tif", label_path, "*.tif", patch_shape, is_seg_dataset=False, **kwargs
+        image_paths, None, label_paths, None, patch_shape, is_seg_dataset=False, **kwargs
     )
 
 
-# TODO implement selecting organ
 def get_monuseg_loader(
-    path, patch_shape, batch_size, split, download=False, offsets=None, boundaries=False, binary=False, **kwargs
+    path, patch_shape, batch_size, split, organ_type=None, download=False, offsets=None, boundaries=False, binary=False,
+    **kwargs
 ):
     ds_kwargs, loader_kwargs = util.split_kwargs(
         torch_em.default_segmentation_dataset, **kwargs
     )
     dataset = get_monuseg_dataset(
-        path, patch_shape, split, download=download,
+        path, patch_shape, split, organ_type=organ_type, download=download,
         offsets=offsets, boundaries=boundaries, binary=binary, **ds_kwargs
     )
     loader = torch_em.get_data_loader(dataset, batch_size, **loader_kwargs)
