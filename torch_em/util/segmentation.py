8e5b17f5b5abb5e06a940d45a8be9cf28eaa0df3
anwai98 <anwai.archit@gmail.com>
2023-10-20 10:48:16 +0200

    Add watershed-based segmentation

--- torch_em/util/segmentation.py
+++ torch_em/util/segmentation.py
@@ -1,15 +1,20 @@
 import numpy as np
-import elf.segmentation as elseg
-import vigra
 
+import vigra
+import elf.segmentation as elseg
 from elf.segmentation.utils import normalize_input
+
 from skimage.measure import label
+from skimage.filters import gaussian
 from skimage.segmentation import watershed
+from skimage.feature import peak_local_max
+from scipy.ndimage import distance_transform_edt
 
 
 #
 # segmentation functionality
 #
+
 
 # could also refactor this into elf
 def size_filter(seg, min_size, hmap=None, with_background=False):
@@ -41,3 +46,56 @@ def connected_components_with_boundaries(foreground, boundaries, threshold=0.5):
     mask = normalize_input(foreground > threshold)
     seg = watershed(boundaries, markers=seeds, mask=mask)
     return seg.astype("uint64")
+
+
+def _size_filter(seg, min_size):
+    ids, sizes = np.unique(seg, return_counts=True)
+    filter_ids = ids[sizes < min_size]
+    seg[np.isin(seg, filter_ids)] = 0
+    return seg
+
+
+def watershed_from_components(
+    boundaries,
+    foreground,
+    min_size,
+    threshold1=0.5,
+    threshold2=0.5,
+):
+    """The default approach:
+    - Subtract the boundaries from the foreground to separate touching objets.
+    - Use the connected components of this as seeds.
+    - Use the thresholded foreground predictions as mask to grow back the pieces
+    lost by subtracting the boundary prediction.
+    """
+    seeds = label((foreground - boundaries) > threshold1)
+    mask = foreground > 0.5
+    seg = watershed(dist, seeds, mask=mask)
+    seg = _size_filter(seg, min_size)
+    return seg
+
+
+def watershed_from_maxima(
+    boundaries,
+    foreground,
+    min_size,
+    min_distance,
+    sigma=1.0,
+):
+    """Find objects via seeded watershed starting from the maxima of the distance transform instead.
+    This has the advantage that objects can be better separated, but it may over-segment
+    if the objects have complex shapes.
+
+    The min_distance parameter controls the minimal distance between seeds, which
+    corresponds to the minimal distance between object centers.
+    """
+    mask = foreground > 0.5
+    boundary_distances = distance_transform_edt(boundaries < 0.1)
+    boundary_distances[~mask] = 0
+    boundary_distances = gaussian(boundary_distances, sigma)
+    seed_points = peak_local_max(boundary_distances, min_distance=min_distance, exclude_border=False)
+    seeds = np.zeros(mask.shape, dtype="uint32")
+    seeds[seed_points[:, 0], seed_points[:, 1]] = np.arange(1, len(seed_points) + 1)
+    seg = watershed(boundaries, markers=seeds, mask=foreground)
+    seg = _size_filter(seg, min_size)
+    return seg
