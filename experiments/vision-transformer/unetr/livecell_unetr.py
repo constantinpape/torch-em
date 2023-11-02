4a9d9f3d38ed6bdba6674b468d818ac7de482843
anwai98 <anwai.archit@gmail.com>
2023-11-02 18:10:25 +0100

    Add training script for livecell with affinities

--- experiments/vision-transformer/unetr/livecell_unetr.py
+++ experiments/vision-transformer/unetr/livecell_unetr.py
@@ -1,10 +1,9 @@
 import os
-import argparse
 import numpy as np
 import pandas as pd
 from glob import glob
 from tqdm import tqdm
-from typing import Tuple, List
+from typing import List
 
 import imageio.v2 as imageio
 from skimage.segmentation import find_boundaries
@@ -14,81 +13,22 @@ import torch
 import torch_em
 from torch_em.util import segmentation
 from torch_em.transform.raw import standardize
-from torch_em.data.datasets import get_livecell_loader
 from torch_em.util.prediction import predict_with_halo
 
-
-def get_unetr_model(
-        model_name: str,
-        source_choice: str,
-        patch_shape: Tuple[int, int],
-        sam_initialization: bool,
-        output_channels: int
-):
-    """Returns the expected UNETR model
-    """
-    if source_choice == "torch-em":
-        from torch_em import model as torch_em_models
-        model = torch_em_models.UNETR(
-            encoder=model_name, out_channels=output_channels,
-            encoder_checkpoint_path="/scratch/usr/nimanwai/models/segment-anything/checkpoints/sam_vit_b_01ec64.pth" if sam_initialization else None
-        )
-
-    elif source_choice == "monai":
-        from monai.networks import nets as monai_models
-        model = monai_models.unetr.UNETR(
-            in_channels=1,
-            out_channels=output_channels,
-            img_size=patch_shape,
-            spatial_dims=2
-        )
-        model.out_channels = 2  # type: ignore
-
-    else:
-        raise ValueError(f"The available UNETR models are either from \"torch-em\" or \"monai\", choose from them instead of - {source_choice}")
-
-    return model
+import common
 
 
 def do_unetr_training(
-        input_path: str,
+        train_loader,
+        val_loader,
         model,
         cell_types: List[str],
-        patch_shape: Tuple[int, int],
         device: torch.device,
-        save_root: str,
         iterations: int,
-        sam_initialization: bool,
-        source_choice: str
+        loss,
+        save_root: str
 ):
     print("Run training for cell types:", cell_types)
-    train_loader = get_livecell_loader(
-        path=input_path,
-        split="train",
-        patch_shape=patch_shape,
-        batch_size=2,
-        cell_types=[cell_types],
-        download=True,
-        boundaries=True,
-        num_workers=8
-    )
-
-    val_loader = get_livecell_loader(
-        path=input_path,
-        split="val",
-        patch_shape=patch_shape,
-        batch_size=1,
-        cell_types=[cell_types],
-        download=True,
-        boundaries=True,
-        num_workers=8
-    )
-
-    _save_root = os.path.join(
-        save_root,
-        f"{source_choice}-sam" if sam_initialization else f"{source_choice}-scratch"
-    ) if save_root is not None else save_root
-
     trainer = torch_em.default_segmentation_trainer(
         name=f"livecell-{cell_types}",
         model=model,
@@ -99,9 +39,9 @@ def do_unetr_training(
         mixed_precision=True,
         log_image_interval=50,
         compile_model=False,
-        save_root=_save_root
+        save_root=save_root,
+        loss=loss
     )
-
     trainer.fit(iterations)
 
 
@@ -111,16 +51,12 @@ def do_unetr_inference(
         model,
         cell_types: List[str],
         root_save_dir: str,
-        sam_initialization: bool,
         save_root: str,
-        source_choice: str
 ):
     for ctype in cell_types:
         test_img_dir = os.path.join(input_path, "images", "livecell_test_images", "*")
 
-        model_ckpt = os.path.join(save_root,
-                                  f"{source_choice}-sam" if sam_initialization else f"{source_choice}-scratch",
-                                  "checkpoints", f"livecell-{ctype}", "best.pt")
+        model_ckpt = os.path.join(save_root, "checkpoints", f"livecell-{ctype}", "best.pt")
         assert os.path.exists(model_ckpt)
 
         model.load_state_dict(torch.load(model_ckpt, map_location=torch.device('cpu'))["model_state"])
@@ -175,7 +111,8 @@ def do_unetr_evaluation(
             continue
 
         fg_set, bd_set = {"CELL TYPE": c1}, {"CELL TYPE": c1}
-        ws1_msa_set, ws2_msa_set, ws1_sa50_set, ws2_sa50_set = {"CELL TYPE": c1}, {"CELL TYPE": c1}, {"CELL TYPE": c1}, {"CELL TYPE": c1}
+        (ws1_msa_set, ws2_msa_set,
+         ws1_sa50_set, ws2_sa50_set) = {"CELL TYPE": c1}, {"CELL TYPE": c1}, {"CELL TYPE": c1}, {"CELL TYPE": c1}
         for c2 in tqdm(cell_types, desc=f"Evaluation on {c1} source models from {_save_dir}"):
             fg_dir = os.path.join(_save_dir, "foreground")
             bd_dir = os.path.join(_save_dir, "boundary")
@@ -250,98 +187,63 @@ def do_unetr_evaluation(
 
 
 def main(args):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    patch_shape = (512, 512)
-    output_channels = 2
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # overwrite to use complex device setups
+    patch_shape = (512, 512)  # patch size used for training on livecell
 
-    all_cell_types = ["A172", "BT474", "BV2", "Huh7", "MCF7", "SHSY5Y", "SkBr3", "SKOV3"]
+    # get the desired loss function for training
+    loss = common.get_loss_function(
+        with_affinities=args.with_affinities  # takes care of calling the loss for training with affinities
+    )
 
-    model = get_unetr_model(
-        model_name=args.model_name,
-        source_choice=args.source_choice,
-        patch_shape=patch_shape,
-        sam_initialization=args.do_sam_ini,
-        output_channels=output_channels
+    # get the desired livecell loaders for training
+    train_loader, val_loader, output_channels = common.get_my_livecell_loaders(
+        args.input, patch_shape, args.cell_type,
+        with_affinities=args.with_affinities  # this takes care of getting the loaders with affinities
+    )
+
+    # get the model for the training and inference on livecell dataset
+    model = common.get_unetr_model(
+        model_name=args.model_name, source_choice=args.source_choice, patch_shape=patch_shape,
+        sam_initialization=args.do_sam_ini, output_channels=output_channels
     )
     model.to(device)
 
+    # determining where to save the checkpoints and tensorboard logs
+    save_root = os.path.join(
+        args.save_root,
+        "affinities" if args.with_affinities else "boundaries",
+        f"{args.source_choice}-sam" if args.do_sam_ini else f"{args.source_choice}-scratch"
+    ) if args.save_root is not None else args.save_root
+
     if args.train:
-        print("2d UNETR training on LiveCell dataset")
+        print("2d UNETR training on LIVECell dataset")
         do_unetr_training(
-            input_path=args.input,
-            model=model,
-            cell_types=args.cell_type,
-            patch_shape=patch_shape,
-            device=device,
-            save_root=args.save_root,
-            iterations=args.iterations,
-            sam_initialization=args.do_sam_ini,
-            source_choice=args.source_choice
+            train_loader=train_loader, val_loader=val_loader, model=model, cell_types=args.cell_type,
+            device=device, save_root=save_root, iterations=args.iterations, loss=loss
         )
 
+    # determines the directory where the predictions will be saved
     root_save_dir = os.path.join(
-        args.save_dir,
-        f"unetr-{args.source_choice}-sam" if args.do_sam_ini else f"unetr-{args.source_choice}-scratch"
+        args.save_dir, f"unetr-{args.source_choice}-sam" if args.do_sam_ini else f"unetr-{args.source_choice}-scratch"
     )
     print("Predictions are saved in", root_save_dir)
 
     if args.predict:
-        print("2d UNETR inference on LiveCell dataset")
+        print("2d UNETR inference on LIVECell dataset")
         do_unetr_inference(
-            input_path=args.input,
-            device=device,
-            model=model,
-            cell_types=all_cell_types,
-            root_save_dir=root_save_dir,
-            sam_initialization=args.do_sam_ini,
-            save_root=args.save_root,
-            source_choice=args.source_choice
+            input_path=args.input, device=device, model=model, cell_types=common.CELL_TYPES,
+            root_save_dir=root_save_dir, save_root=save_root
         )
 
     if args.evaluate:
-        print("2d UNETR evaluation on LiveCell dataset")
+        print("2d UNETR evaluation on LIVECell dataset")
         do_unetr_evaluation(
-            input_path=args.input,
-            cell_types=all_cell_types,
-            root_save_dir=root_save_dir,
-            sam_initialization=args.do_sam_ini,
-            source_choice=args.source_choice
+            input_path=args.input, cell_types=common.CELL_TYPES, root_save_dir=root_save_dir,
+            sam_initialization=args.do_sam_ini, source_choice=args.source_choice
         )
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--train", action='store_true',
-                        help="Enables UNETR training on LiveCell dataset")
-
-    parser.add_argument("--predict", action='store_true',
-                        help="Enables UNETR prediction on LiveCell dataset")
-
-    parser.add_argument("--evaluate", action='store_true',
-                        help="Enables UNETR evaluation on LiveCell dataset")
-
-    parser.add_argument("--source_choice", type=str, default="torch-em",
-                        help="The source where the model comes from, i.e. either torch-em / monai")
-
-    parser.add_argument("-m", "--model_name", type=str, default="vit_b",
-                        help="Name of the ViT to use as the encoder in UNETR")
-
-    parser.add_argument("--do_sam_ini", action='store_true',
-                        help="Enables initializing UNETR with SAM's ViT weights")
-
-    parser.add_argument("-c", "--cell_type", type=str, default=None,
-                        help="Choice of cell-type for doing the training")
-
-    parser.add_argument("-i", "--input", type=str, default="/scratch/usr/nimanwai/data/livecell",
-                        help="Path where the dataset already exists/will be downloaded by the dataloader")
-
-    parser.add_argument("-s", "--save_root", type=str, default="/scratch/usr/nimanwai/models/unetr/",
-                        help="Path where checkpoints and logs will be saved")
-
-    parser.add_argument("--save_dir", type=str, default="/scratch/usr/nimanwai/predictions/unetr",
-                        help="Path to save predictions from UNETR model")
-
-    parser.add_argument("--iterations", type=int, default=100000)
-
+    parser = common.get_parser()
     args = parser.parse_args()
     main(args)
