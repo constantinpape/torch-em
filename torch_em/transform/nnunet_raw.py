c503cb8e919b6929ba944aaf78e14a04ceae4f00
anwai98 <anwai.archit@gmail.com>
2023-11-28 18:38:06 +0100

    Update nnunet raw trafo - single class

--- torch_em/transform/nnunet_raw.py
+++ torch_em/transform/nnunet_raw.py
@@ -2,28 +2,56 @@ import json
 import numpy as np
 
 
-class nnUNetRawTransformBase:
-    """nnUNetRawTransformBase is an interface to implement specific raw transforms for nnUNet.
+class nnUNetRawTransform:
+    """Apply transformation on the raw inputs.
+    Adapted from nnUNetv2's `ImageNormalization`:
+        - https://github.com/MIC-DKFZ/nnUNet/tree/master/nnunetv2/preprocessing/normalization
 
-    Adapted from: https://github.com/MIC-DKFZ/nnUNet/tree/master/nnunetv2/preprocessing/normalization
+    You can use this class to apply the necessary raw transformations on input modalities.
+
+    (Current Support - CT and PET): The inputs should be of dimension 2 * (H * W * D).
+        - The first channel should be CT volume
+        - The second channel should be PET volume
+
+    Here's an example for how to use this class:
+    ```python
+    # Initialize the raw transform.
+    raw_transform = nnUNetRawTransform(plans_file=".../nnUNetPlans.json")
+
+    # Apply transformation on the inputs.
+    patient_vol = np.concatenate(ct_vol, pet_vol)
+    patient_transformed = raw_transform(patient_vol)
+    ```
     """
     def __init__(
             self,
             plans_file: str,
             expected_dtype: type = np.float32,
-            tolerance: float = 1e-8
+            tolerance: float = 1e-8,
+            model_name: str = "3d_fullres"
     ):
         self.expected_dtype = expected_dtype
         self.tolerance = tolerance
 
-        self.intensity_properties = self.load_json(plans_file)
-        self.intensity_properties = self.intensity_properties["foreground_intensity_properties_per_channel"]
+        json_file = self.load_json(plans_file)
+        self.intensity_properties = json_file["foreground_intensity_properties_per_channel"]
+        self.per_channel_scheme = json_file["configurations"][model_name]["normalization_schemes"]
 
     def load_json(self, _file: str):
-        # credits: `batchgenerators.utilities.file_and_folder_operations`
+        # source: `batchgenerators.utilities.file_and_folder_operations`
         with open(_file, 'r') as f:
             a = json.load(f)
         return a
+
+    def ct_transform(self, channel, properties):
+        mean = properties['mean']
+        std = properties['std']
+        lower_bound = properties['percentile_00_5']
+        upper_bound = properties['percentile_99_5']
+
+        transformed_channel = np.clip(channel, lower_bound, upper_bound)
+        transformed_channel = (transformed_channel - mean) / max(std, self.tolerance)
+        return transformed_channel
 
     def __call__(
             self,
@@ -37,52 +65,22 @@ class nnUNetRawTransformBase:
         Returns:
             The transformed raw inputs (the same shape as inputs)
         """
-        raise NotImplementedError("It's a class template for raw transforms from nnUNet. \
-                                  Use a child class that implements the expected raw transform instead")
+        assert raw.shape[0] == len(self.per_channel_scheme), "Number of channels & transforms from data plan must match"
 
+        normalized_channels = []
+        for idxx, (channel_transform, channel) in enumerate(zip(self.per_channel_scheme, raw)):
+            properties = self.intensity_properties[str(idxx)]
 
-class nnUNetCTRawTransform(nnUNetRawTransformBase):
-    """Apply transformation on the raw inputs for CT + PET channels (adapted from nnUNetv2's `CTNormalization`)
+            # get the correct transformation function, this can for example be a method of this class
+            if channel_transform == "CTNormalization":
+                channel = self.ct_transform(channel, properties)
+            elif channel_transform in [
+                "ZScoreNormalization", "NoNormalization", "RescaleTo01Normalization", "RGBTo01Normalization"
+            ]:
+                raise NotImplementedError(f"{channel_transform} is not supported by nnUNetRawTransform yet.")
+            else:
+                raise ValueError(f"Transform is not known: {channel_transform}.")
 
-    You can use this class to apply the necessary raw transformations on CT and PET volume channels.
-    Expectation: The inputs should be of dimension 2 * (H * W * D).
-        - The first channel should be CT volume
-        - The second channel should be PET volume
+            normalized_channels.append(channel)
 
-    Here's an example for how to use this class:
-    ```python
-    # Initialize the raw transform.
-    raw_transform = nnUNetCTRawTransform(plans_file="...nnUNetplans.json")
-
-    # Apply transformation on the inputs.
-    patient_vol = np.concatenate(ct_vol, pet_vol)
-    patient_transformed = raw_transform(patient_vol)
-    ```
-    """
-    def __call__(
-            self,
-            raw: np.ndarray
-    ) -> np.ndarray:
-        assert raw.shape[0] == 2, "The current expectation is channels (modality) first. The fn currently supports for two modalities, namely CT and PET-CT (in the mentioned order)"
-
-        assert self.intensity_properties is not None, \
-            "Intensity properties are required here. Please make sure that you pass the `nnUNetplans.json correctly."
-
-        raw = raw.astype(self.expected_dtype)
-
-        transformed_raw = []
-        # intensity properties for the respective modalities
-        for idx in range(raw.shape[0]):
-            props = self.intensity_properties[str(idx)]
-
-            mean = props['mean']
-            std = props['std']
-            lower_bound = props['percentile_00_5']
-            upper_bound = props['percentile_99_5']
-
-            modality = np.clip(raw[idx, ...], lower_bound, upper_bound)
-            modality = (modality - mean) / max(std, self.tolerance)
-            transformed_raw.append(modality)
-
-        transformed_raw = np.stack(transformed_raw)
-        return transformed_raw
+        return np.stack(normalized_channels)
