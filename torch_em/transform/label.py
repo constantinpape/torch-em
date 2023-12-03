6e52b0372c10225ede571cc85937fc916e0dad76
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2023-12-03 20:44:06 +0100

    Implement new boundary transform and segmentation

--- torch_em/transform/label.py
+++ torch_em/transform/label.py
@@ -1,6 +1,7 @@
 import numpy as np
 import skimage.measure
 import skimage.segmentation
+import vigra
 from scipy.ndimage import distance_transform_edt
 
 from ..util import ensure_array, ensure_spatial_array
@@ -272,3 +273,151 @@ class DistanceTransform:
             return distances
         if self.vector_distances:
             return vector_distances
+
+
+class PerObjectDistances:
+    """Compute the normalized (directed) distances per object in the segmentation.
+    """
+    def __init__(
+        self,
+        distances=True,
+        boundary_distances=True,
+        directed_distances=False,
+        foreground=True,
+        apply_label=True,
+        correct_centers=True,
+        min_size=0,
+        distance_fill_value=1.0,
+    ):
+        if sum([distances, directed_distances, boundary_distances]) == 0:
+            raise ValueError("At least one of distances or directed distances has to be passed.")
+        self.distances = distances
+        self.boundary_distances = boundary_distances
+        self.directed_distances = directed_distances
+        self.foreground = foreground
+
+        self.apply_label = apply_label
+        self.correct_centers = correct_centers
+        self.min_size = min_size
+        self.distance_fill_value = distance_fill_value
+
+    def compute_normalized_object_distances(self, mask, boundaries, bb, center, distances):
+        # Crop the mask and generate array with the correct center.
+        cropped_mask = mask[bb]
+        cropped_center = tuple(ce - b.start for ce, b in zip(center, bb))
+
+        # The centroid might not be inside of the object.
+        # In this case we correct the center by taking the maximum of the distance to the boundary.
+        # Note: the centroid is still the best estimate for the center, as long as it's in the object.
+        correct_center = not cropped_mask[cropped_center]
+
+        # Compute the boundary distances if necessary.
+        # (Either if we need to correct the center, or compute the boundary distances anyways.)
+        if correct_center or self.boundary_distances:
+            # Crop the boundary mask and compute the boundary distances.
+            cropped_boundary_mask = boundaries[bb]
+            boundary_distances = vigra.filters.distanceTransform(cropped_boundary_mask)
+            boundary_distances[~cropped_mask] = 0
+            max_dist_point = np.unravel_index(np.argmax(boundary_distances), boundary_distances.shape)
+
+        # Set the crop center to the max dist point
+        if correct_center:
+            # Find the center (= maximal distance from the boundaries).
+            cropped_center = max_dist_point
+
+        cropped_center_mask = np.zeros_like(cropped_mask, dtype="uint32")
+        cropped_center_mask[cropped_center] = 1
+
+        # Compute the directed distances,
+        this_distances = vigra.filters.vectorDistanceTransform(cropped_center_mask)
+
+        # Keep only the specified distances:
+        if self.distances and self.directed_distances:  # all distances
+            # Compute the undirected ditacnes from directed distances and concatenate,
+            undir = np.linalg.norm(this_distances, axis=-1, keepdims=True)
+            this_distances = np.concatenate([undir, this_distances], axis=-1)
+
+        elif self.distances:  # only undirected distances
+            # Compute the undirected distances from directed distances and keep only them.
+            this_distances = np.linalg.norm(this_distances, axis=-1, keepdims=True)
+
+        elif self.directed_distances:  # only directed distances
+            pass  # We don't have to do anything becasue the directed distances are already computed.
+
+        else:  # this never happens because we check in the constructor
+            assert False
+
+        # Add an extra channel for the boundary distances if specified.
+        if self.boundary_distances:
+            boundary_distances = (boundary_distances[max_dist_point] - boundary_distances)[..., None]
+            this_distances = np.concatenate([this_distances, boundary_distances], axis=-1)
+
+        # Set distances outside of the mask to zero.
+        this_distances[~cropped_mask] = 0
+
+        # Normalize the distances.
+        spatial_axes = tuple(range(mask.ndim))
+        this_distances /= (np.abs(this_distances).max(axis=spatial_axes, keepdims=True) + 1e-7)
+
+        # Set the distance values in the global result.
+        distances[bb][cropped_mask] = this_distances[cropped_mask]
+
+        return distances
+
+    def __call__(self, labels):
+        # Apply label (connected components) if specified.
+        if self.apply_label:
+            labels = skimage.measure.label(labels).astype("uint32")
+        else:  # Otherwise just relabel the segmentation.
+            labels = vigra.analysis.relabelConsecutive(labels)[0].astype("uint32")
+
+        # Filter out small objects if min_size is specified.
+        if self.min_size > 0:
+            ids, sizes = np.unique(labels, return_counts=True)
+            discard_ids = ids[sizes < self.min_size]
+            labels[np.isin(labels, discard_ids)] = 0
+            labels = vigra.analysis.relabelConsecutive(labels)[0].astype("uint32")
+
+        # Compute the boundaries. They will be used to determine the most central point,
+        # and if 'self.boundary_distances is True' to add the boundary distances.
+        boundaries = skimage.segmentation.find_boundaries(labels, mode="inner").astype("uint32")
+
+        # Compute region properties to derive bounding boxes and centers.
+        ndim = labels.ndim
+        props = skimage.measure.regionprops(labels)
+        bounding_boxes = {
+            prop.label: tuple(slice(prop.bbox[i], prop.bbox[i + ndim]) for i in range(ndim))
+            for prop in props
+        }
+
+        # Compute the object centers from centroids.
+        centers = {prop.label: np.round(prop.centroid).astype("int") for prop in props}
+
+        # Compute how many distance channels we have.
+        n_channels = 0
+        if self.distances:  # We need one channel for the overall distances.
+            n_channels += 1
+        if self.boundary_distances:  # We need one channel for the boundary distances.
+            n_channels += 1
+        if self.directed_distances:  # And ndim channels for directed distances.
+            n_channels += ndim
+
+        # Compute the per object distances.
+        distances = np.full(labels.shape + (n_channels,), self.distance_fill_value, dtype="float32")
+        for prop in props:
+            label_id = prop.label
+            mask = labels == label_id
+            distances = self.compute_normalized_object_distances(
+                mask, boundaries, bounding_boxes[label_id], centers[label_id], distances
+            )
+
+        # Bring the distance channel to the first dimension.
+        to_channel_first = (ndim,) + tuple(range(ndim))
+        distances = distances.transpose(to_channel_first)
+
+        # Add the foreground mask as first channel if specified.
+        if self.foreground:
+            binary_labels = (labels > 0).astype("float32")
+            distances = np.concatenate([binary_labels[None], distances], axis=0)
+
+        return distances
