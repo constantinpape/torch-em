4ba7bd2a72d6c92c7e4c4ed537814d80a9d15277
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2023-12-05 15:44:32 +0100

    Add ViT-MAE and Refactor ViT(s) (#172)
    Refactor vision transformers

--- experiments/vision-transformer/unetr/initialize_with_sam.py
+++ experiments/vision-transformer/unetr/initialize_with_sam.py
@@ -3,11 +3,12 @@ from torch_em.model import UNETR
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = UNETR(encoder="vit_h", out_channels=1,
-              encoder_checkpoint_path="/scratch/usr/nimanwai/models/segment-anything/checkpoints/sam_vit_h_4b8939.pth")
+model = UNETR(
+    backbone="mae", encoder="vit_b", out_channels=1, use_sam_stats=False
+)
 model.to(device)
 
-x = torch.randn(1, 3, 1024, 1024).to(device=device)
+x = torch.randn(1, 1, 512, 512).to(device=device)
 
 y = model(x)
 print(y.shape)
