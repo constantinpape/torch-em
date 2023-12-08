822b3ce2edffe38b0e9dec6902b9a70c30a23306
Constantin Pape <constantin.pape@embl.de>
2023-12-08 16:06:17 +0100

    Fix issues in ViT initialization and update UNETR state loading (#180)

--- torch_em/model/vit.py
+++ torch_em/model/vit.py
@@ -39,7 +39,11 @@ class ViT_Sam(ImageEncoderViT):
                 "and then rerun your code."
             )
 
-        super().__init__(embed_dim=embed_dim, **kwargs)
+        super().__init__(
+            embed_dim=embed_dim,
+            global_attn_indexes=global_attn_indexes,
+            **kwargs,
+        )
         self.chunks_for_projection = global_attn_indexes
         self.in_chans = in_chans
         self.embed_dim = embed_dim
