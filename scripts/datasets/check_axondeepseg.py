76f28ab40561157356a1f35f2fde7d073c4fb544
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-01-09 21:31:22 +0100

    Fix AxonDeepSeg (#196)
    Fix issue in axon deep seg loader

--- scripts/datasets/check_axondeepseg.py
+++ scripts/datasets/check_axondeepseg.py
@@ -2,18 +2,21 @@ from torch_em.data.datasets import get_axondeepseg_loader
 from torch_em.util.debug import check_loader
 
 
+ROOT = "/scratch/usr/nimanwai/data/axondeepseg"
+
+
 def check_axondeepseg():
     loader = get_axondeepseg_loader(
-        "./data/axondeepseg", name="sem", patch_shape=(1024, 1024), batch_size=1,
-        one_hot_encoding=True, shuffle=True, download=True
+        ROOT, name="sem", patch_shape=(1024, 1024), batch_size=1, split="train",
+        one_hot_encoding=True, shuffle=True, download=True, val_fraction=0.1
     )
-    check_loader(loader, 5)
+    check_loader(loader, 5, True, True, False, "sem_loader.png")
 
     loader = get_axondeepseg_loader(
-        "./data/axondeepseg", name="tem", patch_shape=(1024, 1024), batch_size=1,
-        one_hot_encoding=True, shuffle=True, download=True
+        ROOT, name="tem", patch_shape=(1024, 1024), batch_size=1, split="train",
+        one_hot_encoding=True, shuffle=True, download=True, val_fraction=0.1
     )
-    check_loader(loader, 5)
+    check_loader(loader, 5, True, True, False, "tem_loader.png")
 
 
 if __name__ == "__main__":
