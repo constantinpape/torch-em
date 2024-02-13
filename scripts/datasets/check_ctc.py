a254a6f162611c5f17bc41dbe630cca0ff8496cf
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-02-13 12:09:24 +0100

    Add test set for CTC dataset (#216)
    * Add test set for ctc dataset

--- scripts/datasets/check_ctc.py
+++ scripts/datasets/check_ctc.py
@@ -1,23 +1,29 @@
-from torch_em.data.datasets.ctc import get_ctc_segmentation_loader, CTC_URLS
+from torch_em.data.datasets.ctc import get_ctc_segmentation_loader, CTC_CHECKSUMS
 from torch_em.util.debug import check_loader
 from torch_em.data.sampler import MinInstanceSampler
 
-ROOT = "/scratch/projects/nim00007/sam/data/ctc/"
+ROOT = "/home/anwai/data/ctc/"
 
 
 # Some of the datasets have partial sparse labels:
 # - Fluo-N2DH-GOWT1
 # - Fluo-N2DL-HeLa
 # Maybe depends on the split?!
-def check_ctc_segmentation():
-    for name in CTC_URLS.keys():
+def check_ctc_segmentation(split):
+    ctc_dataset_names = list(CTC_CHECKSUMS["train"].keys())
+    for name in ctc_dataset_names:
         print("Checking dataset", name)
         loader = get_ctc_segmentation_loader(
-            ROOT, name, (1, 512, 512), 1, download=True,
+            path=ROOT,
+            dataset_name=name,
+            patch_shape=(1, 512, 512),
+            batch_size=1,
+            download=True,
+            split=split,
             sampler=MinInstanceSampler()
         )
-        check_loader(loader, 8, plt=True, save_path="ctc.png")
+        check_loader(loader, 8, plt=True)
 
 
 if __name__ == "__main__":
-    check_ctc_segmentation()
+    check_ctc_segmentation("train")
