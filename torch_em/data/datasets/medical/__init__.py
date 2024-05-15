23d4e07ecd02384636c6b16c9b24d64ffc6473e3
Anwai Archit <anwai.archit@gmail.com>
2024-05-15 23:13:19 +0200

    Add skin lesion segmentation from uwaterloo

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -2,3 +2,4 @@ from .autopet import get_autopet_loader
 from .btcv import get_btcv_dataset, get_btcv_loader
 from .camus import get_camus_dataset, get_camus_loader
 from .siim_acr import get_siim_acr_dataset, get_siim_acr_loader
+from .uwaterloo_skin import get_uwaterloo_skin_dataset, get_uwaterloo_skin_loader
