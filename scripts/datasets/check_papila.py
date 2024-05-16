cef4c4653d524314203d0d27d874d4473f479286
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-05-16 13:38:13 +0200

    Add Papila dataset (#262)
    Add papila dataset

---
+++ scripts/datasets/check_papila.py
@@ -0,0 +1,23 @@
+from torch_em.util.debug import check_loader
+from torch_em.data.datasets.medical import get_papila_loader
+
+
+ROOT = "/media/anwai/ANWAI/data/papila"
+
+
+def check_papila():
+    loader = get_papila_loader(
+        path=ROOT,
+        patch_shape=(256, 256),
+        batch_size=2,
+        resize_inputs=True,
+        task="cup",
+        expert_choice="exp1",
+        download=True,
+    )
+
+    check_loader(loader, 8)
+
+
+if __name__ == "__main__":
+    check_papila()
---
+++ torch_em/data/datasets/medical/papila.py
@@ -0,0 +1,152 @@
+import os
+from glob import glob
+from tqdm import tqdm
+from pathlib import Path
+from typing import Union, Tuple
+
+import numpy as np
+from skimage import draw
+import imageio.v3 as imageio
+
+import torch_em
+from torch_em.transform.generic import ResizeInputs
+
+from .. import util
+from ... import ImageCollectionDataset
+
+
+URL = "https://figshare.com/ndownloader/files/35013982"
+CHECKSUM = "15b053dff496bc8e53eb8a8d0707ef73ba3d56c988eea92b65832c9c82852a7d"
+
+
+def get_papila_data(path, download):
+    os.makedirs(path, exist_ok=True)
+
+    data_dir = os.path.join(path, "PapilaDB-PAPILA-17f8fa7746adb20275b5b6a0d99dc9dfe3007e9f")
+    if os.path.exists(data_dir):
+        return data_dir
+
+    zip_path = os.path.join(path, "papila.zip")
+    util.download_source(path=zip_path, url=URL, download=download, checksum=CHECKSUM)
+    util.unzip(zip_path=zip_path, dst=path)
+
+    return data_dir
+
+
+# contour_to_mask() functions taken from https://github.com/matterport/Mask_RCNN
+def contour_to_mask(cont, img_shape):
+    """Return mask given a contour and the shape of image
+    """
+    c = np.loadtxt(cont)
+    mask = np.zeros(img_shape[:-1], dtype=np.uint8)
+    rr, cc = draw.polygon(c[:, 1], c[:, 0])
+    mask[rr, cc] = 1
+    return mask
+
+
+def _get_papila_paths(path, task, expert_choice, download):
+    data_dir = get_papila_data(path=path, download=download)
+
+    image_paths = sorted(glob(os.path.join(data_dir, "FundusImages", "*.jpg")))
+
+    gt_dir = os.path.join(data_dir, "ground_truth")
+    if os.path.exists(gt_dir):
+        gt_paths = sorted(glob(os.path.join(gt_dir, f"*_{task}.tif")))
+        return image_paths, gt_paths
+
+    os.makedirs(gt_dir, exist_ok=True)
+
+    if task is None:  # we get the binary segmentations for both disc and cup
+        task = "*"
+
+    patient_ids = [Path(image_path).stem for image_path in image_paths]
+
+    input_shape = (1934, 2576, 3)  # shape of the input images
+    gt_paths = []
+    for patient_id in tqdm(patient_ids, desc=f"Converting contours to segmentations for '{expert_choice}'"):
+        gt_contours = sorted(
+            glob(os.path.join(data_dir, "ExpertsSegmentations", "Contours", f"{patient_id}_{task}_{expert_choice}.txt"))
+        )
+
+        assert len(gt_contours) == (4 if task is None else 2)
+
+        for gt_contour in gt_contours:
+            tmp_task = Path(gt_contour).stem.split("_")[1]
+            gt_path = os.path.join(gt_dir, f"{patient_id}_{tmp_task}.tif")
+            gt_paths.append(gt_path)
+            if os.path.exists(gt_path):
+                continue
+
+            semantic_labels = contour_to_mask(cont=gt_contour, img_shape=input_shape)
+            imageio.imwrite(gt_path, semantic_labels)
+
+    return image_paths, gt_paths
+
+
+def get_papila_dataset(
+    path: Union[os.PathLike, str],
+    patch_shape: Tuple[int, int],
+    task: str = "disc",
+    expert_choice: str = "exp1",
+    resize_inputs: bool = False,
+    download: bool = False,
+    **kwargs
+):
+    """Dataset for segmentation of optic cup and optic disc in fundus images.
+
+    The database is located at https://figshare.com/articles/dataset/PAPILA/14798004/2
+
+    The dataset is from Kovalyk et al. - https://doi.org/10.1038/s41597-022-01388-1.
+    Please cite it if you use this dataset for a publication.
+    """
+    assert expert_choice in ["exp1", "exp2"], f"'{expert_choice}' is not a valid expert choice."
+
+    if task is not None:
+        assert task in ["cup", "disc"], f"'{task}' is not a valid task."
+
+    image_paths, gt_paths = _get_papila_paths(path=path, task=task, expert_choice=expert_choice, download=download)
+
+    if resize_inputs:
+        raw_trafo = ResizeInputs(target_shape=patch_shape, is_rgb=True)
+        label_trafo = ResizeInputs(target_shape=patch_shape, is_label=True)
+        patch_shape = None
+    else:
+        patch_shape = patch_shape
+        raw_trafo, label_trafo = None, None
+
+    dataset = ImageCollectionDataset(
+        raw_image_paths=image_paths,
+        label_image_paths=gt_paths,
+        patch_shape=patch_shape,
+        raw_transform=raw_trafo,
+        label_transform=label_trafo,
+        **kwargs
+    )
+
+    return dataset
+
+
+def get_papila_loader(
+    path: Union[os.PathLike, str],
+    patch_shape: Tuple[int, int],
+    batch_size: int,
+    task: str = "disc",
+    expert_choice: str = "exp1",
+    resize_inputs: bool = False,
+    download: bool = False,
+    **kwargs
+):
+    """Dataloader for segmentation of optic cup and optic disc in fundus images. See `get_papila_dataset` for details.
+    """
+    ds_kwargs, loader_kwargs = util.split_kwargs(torch_em.default_segmentation_dataset, **kwargs)
+    dataset = get_papila_dataset(
+        path=path,
+        patch_shape=patch_shape,
+        task=task,
+        expert_choice=expert_choice,
+        resize_inputs=resize_inputs,
+        download=download,
+        **ds_kwargs
+    )
+    loader = torch_em.get_data_loader(dataset=dataset, batch_size=batch_size, **loader_kwargs)
+    return loader
