2c948a3cea7cf38927b9c25a2cb668a9a7630517
anwai98 <anwai.archit@gmail.com>
2024-05-16 22:42:47 +0200

    Add experiments for different minibatch normalizations

---
+++ experiments/misc/normalization/common.py
@@ -0,0 +1,119 @@
+import os
+
+from torch_em.model import UNet2d, UNet3d
+from torch_em.data.datasets import get_livecell_loader, get_plantseg_loader, get_mitoem_loader
+
+
+ROOT = "/scratch/projects/nim00007/sam/data"
+
+
+def get_model(dataset, task, norm):
+    out_chans = 1 if task == "binary" else 2
+
+    if dataset == "livecell":
+        model_choice = "unet2d"
+    elif dataset in ["mitoem", "plantseg"]:
+        model_choice = "unet3d"
+    else:
+        raise ValueError
+
+    if model_choice == "unet2d":
+        model = UNet2d(
+            in_channels=1,
+            out_channels=out_chans,
+            initial_features=64,
+            depth=4,
+            final_activation="Sigmoid",
+            norm=norm,
+        )
+    elif model_choice == "unet3d":
+        model = UNet3d(
+            in_channels=1,
+            out_channels=out_chans,
+            initial_features=64,
+            depth=4,
+            final_activation="Sigmoid",
+            norm=norm,
+        )
+    else:
+        raise ValueError
+
+    return model
+
+
+def get_experiment_name(dataset, task, norm, model_choice):
+    if model_choice == "unet":
+        cfg = "2d" if dataset == "livecell" else "3d"
+    else:
+        raise ValueError
+
+    name = f"{dataset}_{model_choice}{cfg}_{norm}_{task}"
+
+    return name
+
+
+def get_dataloaders(dataset, task):
+    assert task in ["binary", "boundaries"]
+
+    if dataset == "livecell":
+        train_loader = get_livecell_loader(
+            path=os.path.join(ROOT, "livecell"),
+            split="train",
+            patch_shape=(512, 512),
+            batch_size=2,
+            binary=True if task == "binary" else False,
+            boundaries=True if task == "boundaries" else False,
+        )
+        val_loader = get_livecell_loader(
+            path=os.path.join(ROOT, "livecell"),
+            split="val",
+            patch_shape=(512, 512),
+            batch_size=1,
+            binary=True if task == "binary" else False,
+            boundaries=True if task == "boundaries" else False,
+        )
+
+    elif dataset == "plantseg":
+        train_loader = get_plantseg_loader(
+            path=os.path.join(ROOT, "plantseg"),
+            name="root",
+            split="train",
+            patch_shape=(32, 512, 512),
+            batch_size=2,
+            binary=True if task == "binary" else False,
+            boundaries=True if task == "boundaries" else False,
+        )
+
+        val_loader = get_plantseg_loader(
+            path=os.path.join(ROOT, "plantseg"),
+            name="root",
+            split="val",
+            patch_shape=(32, 512, 512),
+            batch_size=1,
+            binary=True if task == "binary" else False,
+            boundaries=True if task == "boundaries" else False,
+        )
+
+    elif dataset == "mitoem":
+        train_loader = get_mitoem_loader(
+            path=os.path.join(ROOT, "mitoem"),
+            splits="train",
+            patch_shape=(32, 512, 512),
+            batch_size=2,
+            binary=True if task == "binary" else False,
+            boundaries=True if task == "boundaries" else False,
+        )
+
+        val_loader = get_mitoem_loader(
+            path=os.path.join(ROOT, "mitoem"),
+            splits="val",
+            patch_shape=(32, 512, 512),
+            batch_size=1,
+            binary=True if task == "binary" else False,
+            boundaries=True if task == "boundaries" else False,
+        )
+
+    else:
+        raise ValueError(f"{dataset} is not a valid dataset choice for this experiment.")
+
+    return train_loader, val_loader
---
+++ experiments/misc/normalization/run_unet.py
@@ -0,0 +1,89 @@
+import os
+
+import torch
+
+import torch_em
+from torch_em.loss import DiceLoss
+
+from common import get_dataloaders, get_model, get_experiment_name
+
+
+SAVE_DIR = "/scratch/projects/nim00007/test/verify_normalization"
+
+
+def run_training(name, model, dataset, task, save_root, device):
+    train_loader, val_loader = get_dataloaders(dataset=dataset, task=task)
+
+    loss = DiceLoss()
+
+    trainer = torch_em.default_segmentation_trainer(
+        name=name,
+        save_root=save_root,
+        model=model,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        loss=loss,
+        metric=loss,
+        device=device,
+        learning_rate=1e-4,
+        mixed_precision=True,
+        compile_model=False,
+        log_image_interval=100,
+    )
+    trainer.fit(iterations=int(1e5))
+
+
+def run_inference():
+    pass
+
+
+def run_evaluation():
+    pass
+
+
+def main(args):
+    phase = args.phase
+    dataset = args.dataset
+    task = args.task
+    norm = args.norm
+
+    assert norm in ["OldDefault", "InstanceNorm"]
+
+    save_root = os.path.join(SAVE_DIR, "models")
+    model = get_model(dataset=dataset, task=task, norm=norm)
+    name = get_experiment_name(dataset=dataset, task=task, norm=norm, model_choice="unet")
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    if phase == "train":
+        run_training(
+            name=name, model=model, dataset=dataset, task=task, save_root=save_root, device=device
+        )
+
+    elif phase == "predict":
+        run_inference(device=device)
+
+    elif phase == "evaluate":
+        run_evaluation()
+
+    else:
+        print(f"'{phase}' is not a valid mode. Choose from 'train' / 'predict' / 'evaluate'.")
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d", "--dataset", required=True, type=str, help="The choice of dataset."
+    )
+    parser.add_argument(
+        "-p", "--phase", required=True, type=str, help="The mode for running the scripts."
+    )
+    parser.add_argument(
+        "-t", "--task", required=True, type=str, help="The type of task for segmentation."
+    )
+    parser.add_argument(
+        "-n", "--norm", required=True, type=str, help="The choice of layer normalization."
+    )
+    args = parser.parse_args()
+    main(args)
