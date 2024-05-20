4439fdeb1219bc65b9c7333f9932e5fe431a81d5
Anwai Archit <anwai.archit@gmail.com>
2024-05-20 20:07:54 +0200

    Add cellpose dataset

---
+++ scripts/datasets/light_microscopy/check_cellpose.py
@@ -0,0 +1,21 @@
+from torch_em.util.debug import check_loader
+from torch_em.data.datasets.light_microscopy import get_cellpose_loader
+
+
+ROOT = "/media/anwai/ANWAI/data/cellpose/"
+
+
+def check_cellpose():
+    loader = get_cellpose_loader(
+        path=ROOT,
+        split="train",
+        patch_shape=(512, 512),
+        batch_size=2,
+        choice="cyto",
+        download=True
+    )
+    check_loader(loader, 8, instance_labels=True)
+
+
+if __name__ == "__main__":
+    check_cellpose()
