1f5ccaa05aacf48ecb6ac0501892ec28bac9d95c
Anwai Archit <anwai.archit@gmail.com>
2024-06-03 20:12:38 +0200

    Remove old visualization scripts

--- scripts/datasets/medical/check_mbh_seg.py
+++ scripts/datasets/medical/check_mbh_seg.py
@@ -19,26 +19,5 @@ def check_mbh_seg():
     check_loader(loader, 8)
 
 
-def visualize_mbh_seg():
-    import os
-    from glob import glob
-    from natsort import natsorted
-
-    import napari
-    from tukra.utils import read_image
-
-    image_paths = natsorted(glob(os.path.join(ROOT, "images", "*.nii.gz")))
-    gt_paths = natsorted(glob(os.path.join(ROOT, r"ground truths", "*.nii.gz")))
-    for image_path, gt_path in zip(image_paths, gt_paths):
-        image = read_image(image_path)
-        gt = read_image(gt_path)
-
-        v = napari.Viewer()
-        v.add_image(image.transpose(2, 0, 1))
-        v.add_labels(gt.transpose(2, 0, 1).astype("uint8"))
-        napari.run()
-
-
 if __name__ == "__main__":
-    # visualize_mbh_seg()
     check_mbh_seg()
