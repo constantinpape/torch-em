686140adf835df7bdf40437e5a2c88e7c9df0b5b
Anwai Archit <anwai.archit@gmail.com>
2024-06-04 17:02:46 +0200

    Add OIMHS dataset

---
+++ scripts/datasets/medical/check_oimhs.py
@@ -0,0 +1,21 @@
+from torch_em.util.debug import check_loader
+from torch_em.data.datasets.medical import get_oimhs_loader
+
+
+ROOT = "/media/anwai/ANWAI/data/oimhs"
+
+
+def check_oimhs():
+    loader = get_oimhs_loader(
+        path=ROOT,
+        patch_shape=(512, 512),
+        batch_size=2,
+        download=False,
+        resize_inputs=False,
+    )
+
+    check_loader(loader, 8)
+
+
+if __name__ == "__main__":
+    check_oimhs()
