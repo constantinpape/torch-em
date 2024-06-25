30adb0b7bf33d32a7669638993005f471ae5f12e
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-06-25 17:07:47 +0200

    Add AMOS dataset (#255)
    * Add AMOS dataset

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -1,4 +1,5 @@
 from .autopet import get_autopet_loader
+from .amos import get_amos_dataset, get_amos_loader
 from .btcv import get_btcv_dataset, get_btcv_loader
 from .busi import get_busi_dataset, get_busi_loader
 from .camus import get_camus_dataset, get_camus_loader
