1f6d00ef073cbcbe1060a8511079756d4a4cc94b
anwai98 <anwai.archit@gmail.com>
2024-10-01 22:34:08 +0200

    Update data storage design for HPA dataset

--- scripts/datasets/light_microscopy/check_hpa.py
+++ scripts/datasets/light_microscopy/check_hpa.py
@@ -10,7 +10,14 @@ sys.path.append("..")
 def check_hpa():
     from util import ROOT
 
-    loader = get_hpa_segmentation_loader(os.path.join(ROOT, "hpa"), "train", (512, 512), 1, download=True)
+    loader = get_hpa_segmentation_loader(
+        path=os.path.join(ROOT, "hpa"),
+        split="train",
+        patch_shape=(1024, 1024),
+        batch_size=1,
+        channels=["protein"],
+        download=True,
+    )
     check_loader(loader, 8, instance_labels=True)
 
 
