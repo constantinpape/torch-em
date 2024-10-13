c5d6fc40b5666558c665db2f5cedbd731e1443d9
anwai98 <anwai.archit@gmail.com>
2024-10-13 16:09:09 +0200

    Add leg 3d us dataset

---
+++ scripts/datasets/medical/check_leg_3d_us.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+from torch_em.util.debug import check_loader
+from torch_em.data.datasets import get_leg_3d_us_loader
+
+
+sys.path.append("..")
+
+
+def check_panorama():
+    from util import ROOT
+
+    loader = get_leg_3d_us_loader(
+        path=os.path.join(ROOT, "leg_3d_us"),
+        patch_shape=(1, 512, 512),
+        batch_size=1,
+        split="train",
+        ndim=2,
+        download=True,
+    )
+    check_loader(loader, 8, plt=True, save_path="./test.png")
+
+
+if __name__ == "__main__":
+    check_panorama()
