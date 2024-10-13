5495842c0fc85546b4f05892e3f52ddccc7cacb6
anwai98 <anwai.archit@gmail.com>
2024-10-13 22:50:25 +0200

    Add docstrings

--- scripts/datasets/medical/check_mbh_seg.py
+++ scripts/datasets/medical/check_mbh_seg.py
@@ -1,19 +1,25 @@
+import os
+import sys
+
 from torch_em.data import MinInstanceSampler
 from torch_em.util.debug import check_loader
 from torch_em.data.datasets.medical import get_mbh_seg_loader
 
-ROOT = "/media/anwai/ANWAI/data/mbh-seg"
+
+sys.path.append("..")
 
 
 def check_mbh_seg():
+    from util import ROOT
+
     loader = get_mbh_seg_loader(
-        path=ROOT,
+        path=os.path.join(ROOT, "mbh_seg"),
         patch_shape=(1, 512, 512),
         ndim=2,
         batch_size=2,
         resize_inputs=False,
-        download=False,
-        sampler=MinInstanceSampler()
+        download=True,
+        sampler=MinInstanceSampler(),
     )
 
     check_loader(loader, 8)
--- torch_em/data/datasets/medical/mbh_seg.py
+++ torch_em/data/datasets/medical/mbh_seg.py
@@ -1,7 +1,17 @@
+"""The MBH Seg dataset contains annotations for intracranial hemorrhages
+in non-contrast CT scans.
+
+This dataset is from the MBH-Seg challenge: https://mbh-seg.com
+- original scans: https://kaggle.com/competitions/rsna-intracranial-hemorrhage-detection
+Please cite these if you use this dataset for your publication.
+"""
+
 import os
 from glob import glob
 from natsort import natsorted
-from typing import Union, Tuple
+from typing import Union, Tuple, List
+
+from torch.utils.data import Dataset, DataLoader
 
 import torch_em
 
@@ -12,12 +22,18 @@ URL = "https://huggingface.co/datasets/WuBiao/BHSD/resolve/main/label_192.zip"
 CHECKSUM = "582bf184af993541a4958a4d209a6a44e3bbe702a5daefaf9fb1733a4e7a6e39"
 
 
-def get_mbh_seg_data(path, download):
-    os.makedirs(path, exist_ok=True)
+def get_mbh_seg_data(path: Union[os.PathLike, str], download: bool = False) -> str:
+    """Download the MBH Seg dataset.
 
+    Args:
+        path: Filepath to a folder where the data is downloaded for further processing.
+        download: Whether to download the data if it is not present.
+    """
     data_dir = os.path.join(path, "label_192")
     if os.path.exists(data_dir):
         return data_dir
+
+    os.makedirs(path, exist_ok=True)
 
     zip_path = os.path.join(path, "label_192.zip")
     util.download_source(path=zip_path, url=URL, download=download, checksum=CHECKSUM)
@@ -26,7 +42,17 @@ def get_mbh_seg_data(path, download):
     return data_dir
 
 
-def _get_mbh_seg_paths(path, download):
+def get_mbh_seg_paths(path: Union[os.PathLike, str], download: bool = False) -> Tuple[List[str], List[str]]:
+    """Get paths to the MBH Seg data.
+
+    Args:
+        path: Filepath to a folder where the data is downloaded for further processing.
+        download: Whether to download the data if it is not present.
+
+    Returns:
+        List of filepaths for the image data.
+        List of filepaths for the label data.
+    """
     data_dir = get_mbh_seg_data(path=path, download=download)
 
     image_paths = natsorted(glob(os.path.join(data_dir, "images", "*.nii.gz")))
@@ -41,15 +67,20 @@ def get_mbh_seg_dataset(
     resize_inputs: bool = False,
     download: bool = False,
     **kwargs
-):
-    """Dataset for segmentation of multi-class intracranial hemorrhages in non-contrast CT scans.
+) -> Dataset:
+    """Get the MBH Seg dataset for intracranial hemorrhage segmentation.
 
-    This dataset is from the MBH-Seg Challenge (https://mbh-seg.com/):
-    - https://kaggle.com/competitions/rsna-intracranial-hemorrhage-detection (original scans)
+    Args:
+        path: Filepath to a folder where the data is downloaded for further processing.
+        patch_shape: The patch shape to use for training.
+        resize_inputs: Whether to resize inputs to the desired patch shape.
+        download: Whether to download the data if it is not present.
+        kwargs: Additional keyword arguments for `torch_em.default_segmentation_dataset`.
 
-    Please cite it if you use the dataset for your publication.
+    Returns:
+        The segmentation dataset.
     """
-    image_paths, gt_paths = _get_mbh_seg_paths(path=path, download=download)
+    image_paths, gt_paths = get_mbh_seg_paths(path=path, download=download)
 
     if resize_inputs:
         resize_kwargs = {"patch_shape": patch_shape, "is_rgb": False}
@@ -57,7 +88,7 @@ def get_mbh_seg_dataset(
             kwargs=kwargs, patch_shape=patch_shape, resize_inputs=resize_inputs, resize_kwargs=resize_kwargs
         )
 
-    dataset = torch_em.default_segmentation_dataset(
+    return torch_em.default_segmentation_dataset(
         raw_paths=image_paths,
         raw_key="data",
         label_paths=gt_paths,
@@ -65,8 +96,6 @@ def get_mbh_seg_dataset(
         patch_shape=patch_shape,
         **kwargs
     )
-
-    return dataset
 
 
 def get_mbh_seg_loader(
@@ -76,13 +105,21 @@ def get_mbh_seg_loader(
     resize_inputs: bool = False,
     download: bool = False,
     **kwargs
-):
-    """Dataloader for segmentation of multi-class intracranial hemorrhages in non-contrast CT scans.
-    See `get_mbh_seg_dataset` for details.
+) -> DataLoader:
+    """Get the MBH Seg dataloader for intracranial hemorrhage segmentation.
+
+    Args:
+        path: Filepath to a folder where the data is downloaded for further processing.
+        patch_shape: The patch shape to use for training.
+        resize_inputs: Whether to resize inputs to the desired patch shape.
+        download: Whether to download the data if it is not present.
+        kwargs: Additional keyword arguments for `torch_em.default_segmentation_dataset` or for the PyTorch DataLoader.
+
+    Returns:
+        The DataLoader.
     """
     ds_kwargs, loader_kwargs = util.split_kwargs(torch_em.default_segmentation_dataset, **kwargs)
     dataset = get_mbh_seg_dataset(
         path=path, patch_shape=patch_shape, resize_inputs=resize_inputs, download=download, **ds_kwargs
     )
-    loader = torch_em.get_data_loader(dataset=dataset, batch_size=batch_size, **loader_kwargs)
-    return loader
+    return torch_em.get_data_loader(dataset=dataset, batch_size=batch_size, **loader_kwargs)
