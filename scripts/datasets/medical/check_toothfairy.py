da13a3aa65fe747cb51b47c25e5ced095b6a01ca
Anwai Archit <anwai.archit@gmail.com>
2024-10-14 19:35:13 +0200

    Merge toothfairy 1 and 2 in one place

--- scripts/datasets/medical/check_toothfairy.py
+++ scripts/datasets/medical/check_toothfairy.py
@@ -1,18 +1,26 @@
+import os
+import sys
+
 from torch_em.util.debug import check_loader
 from torch_em.data import MinInstanceSampler
 from torch_em.data.datasets.medical import get_toothfairy_loader
 
 
-ROOT = "/scratch/share/cidas/cca/data/toothfairy/"
+sys.path.append("..")
 
 
 def check_toothfairy():
+    # from util import ROOT
+    ROOT = "/media/anwai/ANWAI/data"
+
     loader = get_toothfairy_loader(
-        path=ROOT,
+        path=os.path.join(ROOT, "toothfairy"),
         patch_shape=(1, 512, 512),
         ndim=2,
         batch_size=2,
-        sampler=MinInstanceSampler()
+        version="v1",
+        resize_inputs=False,
+        sampler=MinInstanceSampler(),
     )
 
     check_loader(loader, 8, plt=True, save_path="./toothfairy.png")
