91074c25457616bdf973b7a6c12bb4ff2332b575
anwai98 <anwai.archit@gmail.com>
2024-10-15 16:11:13 +0200

    Refactor arcade dataset scripts

--- scripts/datasets/medical/check_arcade.py
+++ scripts/datasets/medical/check_arcade.py
@@ -1,23 +1,29 @@
+import os
+import sys
+
 from torch_em.util.debug import check_loader
 from torch_em.data import MinInstanceSampler
-from torch_em.data.datasets.medical import get_arcade_loader
+from torch_em.data.datasets import get_arcade_loader
 
 
-ROOT = "/media/anwai/ANWAI/data/arcade"
+sys.path.append("..")
 
 
 def check_arcade():
+    # from util import ROOT
+    ROOT = "/scratch/share/cidas/cca/data"
+
     loader = get_arcade_loader(
-        path=ROOT,
+        path=os.path.join(ROOT, "arcade"),
         split="test",
         patch_shape=(256, 256),
         batch_size=2,
         download=True,
         task="syntax",
-        resize_inputs=False,
+        resize_inputs=True,
         sampler=MinInstanceSampler(),
     )
-    check_loader(loader, 8)
+    check_loader(loader, 8, plt=True, save_path="./test.png")
 
 
 if __name__ == "__main__":
