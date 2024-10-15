d42d50c124162c119b7a0f163e2f1bd6435b5013
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-10-15 09:56:42 +0200

    Add ToothFairy2 dataset (#285)
    * Add toothfairy2 dataset

--- scripts/datasets/medical/check_toothfairy.py
+++ scripts/datasets/medical/check_toothfairy.py
@@ -1,18 +1,25 @@
+import os
+import sys
+
 from torch_em.util.debug import check_loader
 from torch_em.data import MinInstanceSampler
 from torch_em.data.datasets.medical import get_toothfairy_loader
 
 
-ROOT = "/scratch/share/cidas/cca/data/toothfairy/"
+sys.path.append("..")
 
 
 def check_toothfairy():
+    from util import ROOT
+
     loader = get_toothfairy_loader(
-        path=ROOT,
+        path=os.path.join(ROOT, "toothfairy"),
         patch_shape=(1, 512, 512),
         ndim=2,
         batch_size=2,
-        sampler=MinInstanceSampler()
+        version="v2",
+        resize_inputs=False,
+        sampler=MinInstanceSampler(),
     )
 
     check_loader(loader, 8, plt=True, save_path="./toothfairy.png")
