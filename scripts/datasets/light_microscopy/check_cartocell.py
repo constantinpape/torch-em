8dcb9350b09ae6d1ce52c9eb819570d648dbde00
Anwai Archit <anwai.archit@gmail.com>
2024-10-17 16:49:13 +0200

    Remove hard-coded paths

--- scripts/datasets/light_microscopy/check_cartocell.py
+++ scripts/datasets/light_microscopy/check_cartocell.py
@@ -9,8 +9,7 @@ sys.path.append("..")
 
 
 def check_cartocell():
-    # from util import ROOT
-    ROOT = "/media/anwai/ANWAI/data"
+    from util import ROOT
 
     loader = get_cartocell_loader(
         path=os.path.join(ROOT, "cartocell"),
