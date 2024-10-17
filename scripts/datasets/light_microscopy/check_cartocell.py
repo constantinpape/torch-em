a8c251b1ea3501b3311c1616eb3039de2e78f83c
Anwai Archit <anwai.archit@gmail.com>
2024-10-17 16:48:48 +0200

    Add cartocell dataset

---
+++ scripts/datasets/light_microscopy/check_cartocell.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+from torch_em.util.debug import check_loader
+from torch_em.data.datasets import get_cartocell_loader
+
+
+sys.path.append("..")
+
+
+def check_cartocell():
+    # from util import ROOT
+    ROOT = "/media/anwai/ANWAI/data"
+
+    loader = get_cartocell_loader(
+        path=os.path.join(ROOT, "cartocell"),
+        batch_size=1,
+        patch_shape=(1, 512, 512),
+        download=True,
+    )
+    check_loader(loader, 8)
+
+
+if __name__ == "__main__":
+    check_cartocell()
