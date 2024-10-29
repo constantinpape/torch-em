b010400579d2591cf2a73d5ebdeda88d65d21a57
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-10-29 08:32:00 +0100

    Add LyNSeC data (#396)
    Add lynsec data

--- torch_em/data/datasets/histopathology/__init__.py
+++ torch_em/data/datasets/histopathology/__init__.py
@@ -1,6 +1,7 @@
 from .bcss import get_bcss_loader, get_bcss_dataset
 from .cryonuseg import get_cryonuseg_loader, get_cryonuseg_dataset
 from .lizard import get_lizard_loader, get_lizard_dataset
+from .lynsec import get_lynsec_loader, get_lynsec_dataset
 from .monuseg import get_monuseg_loader, get_monuseg_dataset
 from .monusac import get_monusac_loader, get_monusac_dataset
 from .pannuke import get_pannuke_loader, get_pannuke_dataset
