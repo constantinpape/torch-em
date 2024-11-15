a707c8fcb7c28ca2de03dd4a48118cc911c0fb80
Anwai Archit <anwai.archit@gmail.com>
2024-11-15 22:21:28 +0100

    Add segpc dataset

---
+++ scripts/datasets/light_microscopy/check_segpc.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+from torch_em.util.debug import check_loader
+from torch_em.data.datasets import get_segpc_loader
+
+
+sys.path.append("..")
+
+
+def check_segpc():
+    # from util import ROOT
+    ROOT = "/media/anwai/ANWAI/data"
+
+    loader = get_segpc_loader(
+        path=os.path.join(ROOT, "segpc"),
+        batch_size=2,
+        patch_shape=(512, 512),
+        split="validation",
+    )
+
+    check_loader(loader, 8)
+
+
+if __name__ == "__main__":
+    check_segpc()
