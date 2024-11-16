41bd0dd5ef53db128943c38e6fbad5354514fc8d
Anwai Archit <anwai.archit@gmail.com>
2024-11-16 20:44:27 +0100

    Remove hard-coded paths

--- scripts/datasets/histopathology/check_nuinsseg.py
+++ scripts/datasets/histopathology/check_nuinsseg.py
@@ -9,8 +9,7 @@ sys.path.append("..")
 
 
 def check_nuinsseg():
-    # from util import ROOT
-    ROOT = "/media/anwai/ANWAI/data"
+    from util import ROOT
 
     loader = get_nuinsseg_loader(
         path=os.path.join(ROOT, "nuinsseg"),
