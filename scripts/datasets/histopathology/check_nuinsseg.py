ad43b7cb1ecd70e623e3d05850765d3d026fe605
Anwai Archit <anwai.archit@gmail.com>
2024-11-16 20:44:13 +0100

    Add nuinsseg dataset

---
+++ scripts/datasets/histopathology/check_nuinsseg.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+from torch_em.util.debug import check_loader
+from torch_em.data.datasets import get_nuinsseg_loader
+
+
+sys.path.append("..")
+
+
+def check_nuinsseg():
+    # from util import ROOT
+    ROOT = "/media/anwai/ANWAI/data"
+
+    loader = get_nuinsseg_loader(
+        path=os.path.join(ROOT, "nuinsseg"),
+        patch_shape=(512, 512),
+        batch_size=2,
+        download=True,
+    )
+
+    check_loader(loader, 8, instance_labels=True)
+
+
+if __name__ == "__main__":
+    check_nuinsseg()
--- torch_em/data/datasets/histopathology/__init__.py
+++ torch_em/data/datasets/histopathology/__init__.py
@@ -4,6 +4,7 @@ from .lizard import get_lizard_loader, get_lizard_dataset
 from .lynsec import get_lynsec_loader, get_lynsec_dataset
 from .monuseg import get_monuseg_loader, get_monuseg_dataset
 from .monusac import get_monusac_loader, get_monusac_dataset
+from .nuinsseg import get_nuinsseg_loader, get_nuinsseg_dataset
 from .pannuke import get_pannuke_loader, get_pannuke_dataset
 from .srsanet import get_srsanet_loader, get_srsanet_dataset
 from .tnbc import get_tnbc_loader, get_tnbc_dataset
