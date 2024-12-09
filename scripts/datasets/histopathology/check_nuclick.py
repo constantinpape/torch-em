48f0005af9ec58475664bbb3e8a42d3b8e12c902
Anwai Archit <anwai.archit@gmail.com>
2024-12-09 13:21:23 +0100

    Add nuclick dataset

---
+++ scripts/datasets/histopathology/check_nuclick.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+from torch_em.util.debug import check_loader
+from torch_em.data.datasets import get_nuclick_loader
+
+
+sys.path.append("..")
+
+
+def check_nuclick():
+    # from util import ROOT
+    ROOT = "/home/anwai/data"
+
+    loader = get_nuclick_loader(
+        path=os.path.join(ROOT, "nuclick"),
+        patch_shape=(512, 512),
+        batch_size=2,
+        download=False,
+        split="Train",
+    )
+
+    check_loader(loader, 8)
+
+
+if __name__ == "__main__":
+    check_nuclick()
