e6a312151ee263a6e44c46b59258a6de230c1adc
Anwai Archit <anwai.archit@gmail.com>
2024-12-09 13:21:44 +0100

    Remove hard-coded paths

--- scripts/datasets/histopathology/check_nuclick.py
+++ scripts/datasets/histopathology/check_nuclick.py
@@ -9,8 +9,7 @@ sys.path.append("..")
 
 
 def check_nuclick():
-    # from util import ROOT
-    ROOT = "/home/anwai/data"
+    from util import ROOT
 
     loader = get_nuclick_loader(
         path=os.path.join(ROOT, "nuclick"),
