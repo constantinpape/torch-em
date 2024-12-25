bcdc29bf84d888cf825cf5db87d4224e6b688234
Anwai Archit <anwai.archit@gmail.com>
2024-12-25 13:07:07 +0100

    Add data splits for motum dataset

--- scripts/datasets/medical/check_motum.py
+++ scripts/datasets/medical/check_motum.py
@@ -9,16 +9,18 @@ sys.path.append("..")
 
 
 def check_motum():
-    from util import ROOT
+    # from util import ROOT
+    ROOT = "/media/anwai/ANWAI/data"
 
     loader = get_motum_loader(
         path=os.path.join(ROOT, "motum"),
         batch_size=1,
-        patch_shape=(8, 512, 512),
-        ndim=3,
+        patch_shape=(1, 512, 512),
+        ndim=2,
+        split="train",
         resize_inputs=True,
-        modality="t1ce",
-        download=False,
+        modality="flair",
+        download=True,
     )
 
     check_loader(loader, 8)
