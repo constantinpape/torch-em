b4ab211354b98961b889ae6dac8daa069d609825
Anwai Archit <anwai.archit@gmail.com>
2024-12-25 21:11:41 +0100

    Add resize inputs for JSRT data

--- torch_em/data/datasets/medical/jsrt.py
+++ torch_em/data/datasets/medical/jsrt.py
@@ -118,6 +118,7 @@ def get_jsrt_dataset(
     patch_shape: Tuple[int, int],
     split: Literal['train', 'test'],
     choice: Optional[Literal['Segmentation01', 'Segmentation02']] = None,
+    resize_inputs: bool = False,
     download: bool = False,
     **kwargs
 ) -> Dataset:
@@ -128,6 +129,7 @@ def get_jsrt_dataset(
         patch_shape: The patch shape to use for training.
         split: The data split to use. Either 'train', or 'test'.
         choice: The choice of data subset. Either 'Segmentation01' or 'Segmentation02'.
+        resize_inputs: Whether to resize the inputs.
         download: Whether to download the data if it is not present.
         kwargs: Additional keyword arguments for `torch_em.default_segmentation_dataset`.
 
@@ -135,6 +137,12 @@ def get_jsrt_dataset(
         The segmentation dataset.
     """
     image_paths, gt_paths = get_jsrt_paths(path, split, choice, download)
+
+    if resize_inputs:
+        resize_kwargs = {"patch_shape": patch_shape, "is_rgb": False}
+        kwargs, patch_shape = util.update_kwargs_for_resize_trafo(
+            kwargs=kwargs, patch_shape=patch_shape, resize_inputs=resize_inputs, resize_kwargs=resize_kwargs
+        )
 
     return torch_em.default_segmentation_dataset(
         raw_paths=image_paths, raw_key=None, label_paths=gt_paths, label_key=None, patch_shape=patch_shape, **kwargs
@@ -147,6 +155,7 @@ def get_jsrt_loader(
     batch_size: int,
     split: Literal['train', 'test'],
     choice: Optional[Literal['Segmentation01', 'Segmentation02']] = None,
+    resize_inputs: bool = False,
     download: bool = False,
     **kwargs
 ) -> DataLoader:
@@ -158,6 +167,7 @@ def get_jsrt_loader(
         batch_size: The batch size for training.
         split: The data split to use. Either 'train', or 'test'.
         choice: The choice of data subset. Either 'Segmentation01' or 'Segmentation02'.
+        resize_inputs: Whether to resize the inputs.
         download: Whether to download the data if it is not present.
         kwargs: Additional keyword arguments for `torch_em.default_segmentation_dataset` or for the PyTorch DataLoader.
 
@@ -165,5 +175,5 @@ def get_jsrt_loader(
         The DataLoader.
     """
     ds_kwargs, loader_kwargs = util.split_kwargs(torch_em.default_segmentation_dataset, **kwargs)
-    dataset = get_jsrt_dataset(path, patch_shape, split, choice, download, **ds_kwargs)
+    dataset = get_jsrt_dataset(path, patch_shape, split, choice, resize_inputs, download, **ds_kwargs)
     return torch_em.get_data_loader(dataset=dataset, batch_size=batch_size, **loader_kwargs)
