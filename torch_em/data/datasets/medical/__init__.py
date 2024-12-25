37e47399cc0d67065e6f733d1f66160209fa2d20
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-12-25 14:14:18 +0100

    Add AMD-SD dataset (#451)
    Add new OCT dataset

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -1,7 +1,8 @@
 from .acdc import get_acdc_dataset, get_acdc_loader
 from .acouslic_ai import get_acouslic_ai_dataset, get_acouslic_ai_loader
-from .autopet import get_autopet_dataset, get_autopet_loader
+from .amd_sd import get_amd_sd_dataset, get_amd_sd_loader
 from .amos import get_amos_dataset, get_amos_loader
+from .autopet import get_autopet_dataset, get_autopet_loader
 from .btcv import get_btcv_dataset, get_btcv_loader
 from .busi import get_busi_dataset, get_busi_loader
 from .camus import get_camus_dataset, get_camus_loader
