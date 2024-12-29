de13fb60f6542030652ae696c1714e8939085a11
Anwai Archit <anwai.archit@gmail.com>
2024-12-29 17:09:31 +0100

    Remove hard-coded paths

--- scripts/datasets/histopathology/check_consep.py
+++ scripts/datasets/histopathology/check_consep.py
@@ -9,8 +9,7 @@ sys.path.append("..")
 
 
 def check_consep():
-    # from util import ROOT
-    ROOT = "/media/anwai/ANWAI/data"
+    from util import ROOT
 
     loader = get_consep_loader(
         path=os.path.join(ROOT, "consep"),
