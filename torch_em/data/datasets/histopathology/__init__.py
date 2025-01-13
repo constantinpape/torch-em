983f9cc554ce9ad84461ae28db8f976803d7f301
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2025-01-13 15:59:53 +0100

    Add GlaS dataset (#474)
    Add gland segmentation dataset relevant for histopathology

--- torch_em/data/datasets/histopathology/__init__.py
+++ torch_em/data/datasets/histopathology/__init__.py
@@ -2,6 +2,7 @@ from .bcss import get_bcss_loader, get_bcss_dataset
 from .consep import get_consep_loader, get_consep_dataset
 from .cpm import get_cpm_loader, get_cpm_dataset
 from .cryonuseg import get_cryonuseg_loader, get_cryonuseg_dataset
+from .glas import get_glas_loader, get_glas_dataset
 from .janowczyk import get_janowczyk_loader, get_janowczyk_dataset
 from .lizard import get_lizard_loader, get_lizard_dataset
 from .lynsec import get_lynsec_loader, get_lynsec_dataset
