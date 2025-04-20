25b4c751fd41886f357069764a44db46c0660ad3
anwai98 <anwai.archit@gmail.com>
2025-04-20 12:38:55 +0200

    Restore livecell training scripts

--- experiments/unet-segmentation/livecell/train_boundaries.py
+++ experiments/unet-segmentation/livecell/train_boundaries.py
@@ -5,45 +5,27 @@ from torch_em.data.datasets import get_livecell_loader
 
 
 def train_boundaries(args):
-    # Get the UNet model.
     n_out = 2
-    model = UNet2d(
-        in_channels=1,
-        out_channels=n_out,
-        initial_features=64,
-        final_activation="Sigmoid",
-        norm="BatchRenorm",  # HACK: testing a new custom normalization.
-    )
+    model = UNet2d(in_channels=1, out_channels=n_out, initial_features=64,
+                   final_activation="Sigmoid")
 
-    # Get the dataloaders.
     patch_shape = (512, 512)
     train_loader = get_livecell_loader(
-        path=args.input,
-        patch_shape=patch_shape,
-        split="train",
-        download=True,
-        boundaries=True,
-        batch_size=args.batch_size,
-        cell_types=None if args.cell_type is None else [args.cell_type],
+        args.input, patch_shape, "train",
+        download=True, boundaries=True, batch_size=args.batch_size,
+        cell_types=None if args.cell_type is None else [args.cell_type]
     )
     val_loader = get_livecell_loader(
-        path=args.input,
-        patch_shape=patch_shape,
-        split="val",
-        boundaries=True,
-        batch_size=args.batch_size,
-        cell_types=None if args.cell_type is None else [args.cell_type],
+        args.input, patch_shape, "val",
+        boundaries=True, batch_size=args.batch_size,
+        cell_types=None if args.cell_type is None else [args.cell_type]
     )
-
-    # Get the loss function and other stuff for training.
     loss = torch_em.loss.DiceLoss()
 
     cell_type = args.cell_type
     name = "livecell-boundary-model"
     if cell_type is not None:
         name = f"{name}-{cell_type}"
-
-    # Get the trainer.
     trainer = torch_em.default_segmentation_trainer(
         name=name,
         model=model,
@@ -55,7 +37,6 @@ def train_boundaries(args):
         device=torch.device("cuda"),
         mixed_precision=True,
         log_image_interval=50,
-        compile_model=False,
     )
     trainer.fit(iterations=args.n_iterations)
 
@@ -66,25 +47,17 @@ def check_loader(args, train=True, val=True, n_images=5):
     if train:
         print("Check train loader")
         loader = get_livecell_loader(
-            path=args.input,
-            patch_shape=patch_shape,
-            split="train",
-            download=True,
-            boundaries=True,
-            batch_size=1,
-            cell_types=None if args.cell_type is None else [args.cell_type],
+            args.input, patch_shape, "train",
+            download=True, boundaries=True, batch_size=1,
+            cell_types=None if args.cell_type is None else [args.cell_type]
         )
         check_loader(loader, n_images)
     if val:
         print("Check val loader")
         loader = get_livecell_loader(
-            path=args.input,
-            patch_shape=patch_shape,
-            split="val",
-            download=True,
-            boundaries=True,
-            batch_size=1,
-            cell_types=None if args.cell_type is None else [args.cell_type],
+            args.input, patch_shape, "val",
+            download=True, boundaries=True, batch_size=1,
+            cell_types=None if args.cell_type is None else [args.cell_type]
         )
         check_loader(loader, n_images)
 
