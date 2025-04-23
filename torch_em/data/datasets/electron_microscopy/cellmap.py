c953df46bbb03135741fe11ed87c9059a4d743b7
anwai98 <anwai.archit@gmail.com>
2025-04-23 16:35:47 +0200

    Minor fix to handle large label crops in CellMap

--- torch_em/data/datasets/electron_microscopy/cellmap.py
+++ torch_em/data/datasets/electron_microscopy/cellmap.py
@@ -280,11 +280,18 @@ def _download_cellmap_data(path, crops, resolution, padding, download=False):
                 return label_name
 
             if gt_level is not None:
-                with ThreadPoolExecutor() as pool:
-                    futures = {pool.submit(_fetch_and_write_label, name): name for name in crop_group_inventory}
-                    for future in as_completed(futures):
-                        label_name = future.result()
-                        log.info(f"Saved ground truth crop '{crop.id}' for '{label_name}'.")
+                # For this one (large) crop in particular, we store labels in serial
+                # as multiple threads cannot handle it and silently crash.
+                if crop.id == 247:
+                    for name in crop_group_inventory:
+                        _fetch_and_write_label(name)
+                        log.info(f"Saved ground truth crop '{crop.id}' for '{name}'.")
+                else:
+                    with ThreadPoolExecutor() as pool:
+                        futures = {pool.submit(_fetch_and_write_label, name): name for name in crop_group_inventory}
+                        for future in as_completed(futures):
+                            label_name = future.result()
+                            log.info(f"Saved ground truth crop '{crop.id}' for '{label_name}'.")
 
         log.info(f"Saved crop '{crop.id}' to '{crop_path}'.")
         log = log.unbind("crop_id", "dataset")
