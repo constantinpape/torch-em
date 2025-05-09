1b66a2b08c203e9545321cc0fd6c4149b26cd28d
Anwai Archit <anwai.archit@gmail.com>
2025-05-09 15:34:02 +0200

    Clean irrelevant code

--- scripts/datasets/light_microscopy/check_orgaextractor.py
+++ scripts/datasets/light_microscopy/check_orgaextractor.py
@@ -23,34 +23,5 @@ def check_orgaextractor():
     check_loader(loader, 8, instance_labels=True)
 
 
-def test_stuff(fraction=0.1):
-    import torch_em
-    from torch_em.data.datasets import get_livecell_dataset, livecell
-
-    datasets = []
-    for cell_type in livecell.CELL_TYPES:
-
-        image_paths, _ = livecell.get_livecell_paths(
-            path=...,
-            split="train",
-            cell_types=[cell_type]
-        )
-
-        datasets.append(
-            get_livecell_dataset(
-                path=...,
-                split="train",
-                patch_shape=...,
-                cell_types=[cell_type],
-                n_samples=len(image_paths) * fraction,
-            )
-        )
-
-    from torch_em.data import ConcatDataset
-    final_dataset = ConcatDataset(datasets)
-
-    final_loader = torch_em.get_data_loader(final_dataset, batch_size=2)
-
-
 if __name__ == "__main__":
     check_orgaextractor()
