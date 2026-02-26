dcf1d9395c5a82195a2d5ea32de435ba47572b0d
GOESTERN-1107078 <u25914@glogin10.usr.hpc.gwdg.de>
2026-02-26 14:21:17 +0100

    clDice loss and metric

---
+++ torch_em/loss/cdice_loss.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# From "clDice -- A Novel Topology-Preserving Loss Function for Tubular Structure Segmentation":
+# https://arxiv.org/abs/2003.07311
+
+class SoftSkeletonize(torch.nn.Module):
+
+    def __init__(self, num_iter=40):
+
+        super(SoftSkeletonize, self).__init__()
+        self.num_iter = num_iter
+
+    def soft_erode(self, img):
+
+        if len(img.shape)==4:
+            p1 = -F.max_pool2d(-img, (3,1), (1,1), (1,0))
+            p2 = -F.max_pool2d(-img, (1,3), (1,1), (0,1))
+            return torch.min(p1,p2)
+        elif len(img.shape)==5:
+            p1 = -F.max_pool3d(-img,(3,1,1),(1,1,1),(1,0,0))
+            p2 = -F.max_pool3d(-img,(1,3,1),(1,1,1),(0,1,0))
+            p3 = -F.max_pool3d(-img,(1,1,3),(1,1,1),(0,0,1))
+            return torch.min(torch.min(p1, p2), p3)
+
+    def soft_dilate(self, img):
+
+        if len(img.shape)==4:
+            return F.max_pool2d(img, (3,3), (1,1), (1,1))
+        elif len(img.shape)==5:
+            return F.max_pool3d(img,(3,3,3),(1,1,1),(1,1,1))
+
+    def soft_open(self, img):
+        
+        return self.soft_dilate(self.soft_erode(img))
+
+    def soft_skel(self, img):
+
+        img1 = self.soft_open(img)
+        skel = F.relu(img-img1)
+
+        for j in range(self.num_iter):
+            img = self.soft_erode(img)
+            img1 = self.soft_open(img)
+            delta = F.relu(img-img1)
+            skel = skel + F.relu(delta - skel * delta)
+
+        return skel
+
+    def forward(self, img):
+
+        return self.soft_skel(img)
+    
+class soft_cldice(nn.Module):
+    def __init__(self, iter_=3, smooth = 1., exclude_background=False):
+        super(soft_cldice, self).__init__()
+        self.iter = iter_
+        self.smooth = smooth
+        self.soft_skeletonize = SoftSkeletonize(num_iter=10)
+        self.exclude_background = exclude_background
+
+    def forward(self, y_true, y_pred):
+        if self.exclude_background:
+            y_true = y_true[:, 1:, :, :]
+            y_pred = y_pred[:, 1:, :, :]
+        skel_pred = self.soft_skeletonize(y_pred)
+        skel_true = self.soft_skeletonize(y_true)
+        tprec = (torch.sum(torch.multiply(skel_pred, y_true))+self.smooth)/(torch.sum(skel_pred)+self.smooth)    
+        tsens = (torch.sum(torch.multiply(skel_true, y_pred))+self.smooth)/(torch.sum(skel_true)+self.smooth)    
+        cl_dice = 1.- 2.0*(tprec*tsens)/(tprec+tsens)
+        return cl_dice
+
+
+def soft_dice(y_true, y_pred):
+    """[function to compute dice loss]
+
+    Args:
+        y_true ([float32]): [ground truth image]
+        y_pred ([float32]): [predicted image]
+
+    Returns:
+        [float32]: [loss value]
+    """
+    smooth = 1
+    intersection = torch.sum((y_true * y_pred))
+    coeff = (2. *  intersection + smooth) / (torch.sum(y_true) + torch.sum(y_pred) + smooth)
+    return (1. - coeff)
+
+
+class soft_dice_cldice(nn.Module):
+    def __init__(self, iter_=3, alpha=0.5, smooth = 1., exclude_background=False):
+        super(soft_dice_cldice, self).__init__()
+        self.iter = iter_
+        self.smooth = smooth
+        self.alpha = alpha
+        self.soft_skeletonize = SoftSkeletonize(num_iter=10)
+        self.exclude_background = exclude_background
+
+    def forward(self, y_true, y_pred):
+        if self.exclude_background:
+            y_true = y_true[:, 1:, :, :]
+            y_pred = y_pred[:, 1:, :, :]
+        dice = soft_dice(y_true, y_pred)
+        skel_pred = self.soft_skeletonize(y_pred)
+        skel_true = self.soft_skeletonize(y_true)
+        tprec = (torch.sum(torch.multiply(skel_pred, y_true))+self.smooth)/(torch.sum(skel_pred)+self.smooth)    
+        tsens = (torch.sum(torch.multiply(skel_true, y_pred))+self.smooth)/(torch.sum(skel_true)+self.smooth)    
+        cl_dice = 1.- 2.0*(tprec*tsens)/(tprec+tsens)
+        return (1.0-self.alpha)*dice+self.alpha*cl_dice
+
+
