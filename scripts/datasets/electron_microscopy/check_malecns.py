881831a610ae5ae9095f8d729599835ddb84b4cd
Anwai Archit <anwai.archit@gmail.com>
2026-03-13 10:59:17 +0100

    Add MaleCNS data

---
+++ scripts/datasets/electron_microscopy/check_malecns.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+from torch_em.data.datasets import get_malecns_loader
+from torch_em.util.debug import check_loader
+
+sys.path.append("..")
+
+
+def check_malecns():
+    from util import ROOT
+
+    loader = get_malecns_loader(
+        path=os.path.join(ROOT, "malecns"),
+        patch_shape=(32, 256, 256),
+        batch_size=1,
+        download=True,
+    )
+    check_loader(loader, 4, instance_labels=True, plt=True, save_path="./malecns.png")
+
+
+if __name__ == "__main__":
+    check_malecns()
