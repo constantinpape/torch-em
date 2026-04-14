5bb2e737b51b564b4fd434c0ecb5ad1c24ed90c7
Anwai Archit <anwai.archit@gmail.com>
2026-04-14 16:12:39 +0200

    Add SegPath dataset (#660)
    * Add SegPath dataset
    
    * Update SegPath dataset description
    
    * Store local plots for visualization
    
    * Handle compressed SegPath metadata
    
    * Add more changes to plot storing code

---
+++ scripts/datasets/histopathology/check_segpath.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+from torch_em.util.debug import check_loader
+from torch_em.data.datasets import get_segpath_loader
+
+
+sys.path.append("..")
+
+
+def check_segpath():
+    # from util import ROOT
+    ROOT = "/mnt/vast-nhr/projects/cidas/cca/data"
+
+    loader = get_segpath_loader(
+        path=os.path.join(ROOT, "segpath"),
+        patch_shape=(512, 512),
+        batch_size=1,
+        cell_types="epithelium",
+        split="train",
+        download=True,
+    )
+
+    check_loader(loader, 8, instance_labels=False, rgb=True, plt=True, save_path="segpath.png")
+
+
+if __name__ == "__main__":
+    check_segpath()
