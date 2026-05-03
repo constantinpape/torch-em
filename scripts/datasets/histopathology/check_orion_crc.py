2abfa06ad1ed35277ebcd5d29025166fd624b77c
Anwai Archit <anwai.archit@gmail.com>
2026-05-03 16:14:11 +0200

    Add more details to docstrings

--- scripts/datasets/histopathology/check_orion_crc.py
+++ scripts/datasets/histopathology/check_orion_crc.py
@@ -8,21 +8,26 @@ from torch_em.data.datasets import get_orion_crc_loader
 sys.path.append("..")
 
 
-def check_orion_crc():
+def check_orion_crc(modality, label_type):
     from util import ROOT
 
     loader = get_orion_crc_loader(
         path=os.path.join(ROOT, "orion_crc"),
         split="train",
-        modality="he",
-        label_type="instances",
+        modality=modality,
+        label_type=label_type,
         patch_shape=(512, 512),
         batch_size=2,
         download=False,
     )
 
-    check_loader(loader, 8, instance_labels=True, rgb=True)
+    is_instance = label_type == "instances"
+    print(f"ORION-CRC | modality={modality} | label_type={label_type} | #batches={len(loader)}")
+    check_loader(loader, 8, instance_labels=is_instance, rgb=modality == "he")
 
 
 if __name__ == "__main__":
-    check_orion_crc()
+    check_orion_crc(modality="he", label_type="instances")
+    check_orion_crc(modality="he", label_type="semantic")
+    check_orion_crc(modality="mif", label_type="instances")
+    check_orion_crc(modality="mif", label_type="semantic")
