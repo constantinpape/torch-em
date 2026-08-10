6340d74eca33c7e9e84dd0e9782b0e5e4dac5c5b
Anwai Archit <anwai.archit@gmail.com>
2026-08-10 20:26:47 +0200

    Add MRose bacteria dataset

--- torch_em/data/datasets/light_microscopy/__init__.py
+++ torch_em/data/datasets/light_microscopy/__init__.py
@@ -61,6 +61,7 @@ from .microbeseg import get_microbeseg_loader, get_microbeseg_dataset
 from .morphonet import get_morphonet_loader, get_morphonet_dataset
 from .morphoseg import get_morphoseg_loader, get_morphoseg_dataset
 from .mouse_embryo import get_mouse_embryo_loader, get_mouse_embryo_dataset
+from .mrose_bacteria import get_mrose_bacteria_loader, get_mrose_bacteria_dataset
 from .myofuse import get_myofuse_loader, get_myofuse_dataset
 from .neurips_cell_seg import (
     get_neurips_cellseg_supervised_loader, get_neurips_cellseg_supervised_dataset,
