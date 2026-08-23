43388bce65b2526ae26a5f1240441d24c2f50d97
Anwai Archit <anwai.archit@gmail.com>
2026-08-23 21:12:52 +0200

    Export the GLySAC histopathology dataset loader (#762)

--- torch_em/data/datasets/histopathology/__init__.py
+++ torch_em/data/datasets/histopathology/__init__.py
@@ -7,6 +7,7 @@ from .cryonuseg import get_cryonuseg_loader, get_cryonuseg_dataset
 from .cytodark0 import get_cytodark0_loader, get_cytodark0_dataset
 from .deepliif import get_deepliif_loader, get_deepliif_dataset
 from .glas import get_glas_loader, get_glas_dataset
+from .glysac import get_glysac_loader, get_glysac_dataset
 from .hest import get_hest_loader, get_hest_dataset
 from .ignite import get_ignite_loader, get_ignite_dataset
 from .janowczyk import get_janowczyk_loader, get_janowczyk_dataset
