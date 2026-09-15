9d7827b9b38fbf2f0ed6b096d68afedf918832b0
anwai98 <anwai.archit@gmail.com>
2026-09-15 13:41:28 +0200

    Add the WORD dataset for 16 abdominal organs

---
+++ scripts/datasets/medical/check_word.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+from torch_em.util.debug import check_loader
+from torch_em.data.datasets.medical.word import get_word_loader
+
+
+sys.path.append("..")
+
+
+def check_word():
+    from util import ROOT
+
+    loader = get_word_loader(
+        path=os.path.join(ROOT, "word"),
+        patch_shape=(1, 512, 512),
+        batch_size=1,
+        split="train",
+        ndim=2,
+        download=True,
+    )
+
+    check_loader(loader, 8, plt=True, save_path="./test.png")
+
+
+if __name__ == "__main__":
+    check_word()
