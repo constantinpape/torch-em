3d110047e04ad93b59af943042aaf85a255aaca1
anwai98 <anwai.archit@gmail.com>
2026-09-15 13:41:18 +0200

    Add the CETUS dataset for left ventricle segmentation

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -22,6 +22,7 @@ from .camus import get_camus_dataset, get_camus_loader
 from .cap_lv import get_cap_lv_dataset, get_cap_lv_loader
 from .cbis_ddsm import get_cbis_ddsm_dataset, get_cbis_ddsm_loader
 from .cc_tumor_heterogeneity import get_cc_tumor_heterogeneity_dataset, get_cc_tumor_heterogeneity_loader
+from .cetus import get_cetus_dataset, get_cetus_loader
 from .chaksu import get_chaksu_dataset, get_chaksu_loader
 from .chaos import get_chaos_dataset, get_chaos_loader
 from .cholecseg8k import get_cholecseg8k_dataset, get_cholecseg8k_loader
