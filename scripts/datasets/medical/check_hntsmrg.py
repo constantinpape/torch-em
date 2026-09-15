78500f35a195028dd8d9c4438d0b5116cc7e0401
anwai98 <anwai.archit@gmail.com>
2026-09-15 13:41:20 +0200

    Add the HNTS-MRG dataset for head and neck tumor segmentation

---
+++ scripts/datasets/medical/check_hntsmrg.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+from torch_em.util.debug import check_loader
+from torch_em.data import MinInstanceSampler
+from torch_em.data.datasets.medical.hntsmrg import get_hntsmrg_loader
+
+
+sys.path.append("..")
+
+
+def check_hntsmrg():
+    from util import ROOT
+
+    loader = get_hntsmrg_loader(
+        path=os.path.join(ROOT, "hntsmrg"),
+        patch_shape=(1, 256, 256),
+        batch_size=1,
+        timepoint="pre",
+        ndim=2,
+        download=True,
+        sampler=MinInstanceSampler(min_num_instances=2),
+    )
+
+    check_loader(loader, 8, plt=True, save_path="./test.png")
+
+
+if __name__ == "__main__":
+    check_hntsmrg()
