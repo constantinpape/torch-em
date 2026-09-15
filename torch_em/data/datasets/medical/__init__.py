9ed2b000a4c5e3c6c2070c7f52d82a36f2413a88
anwai98 <anwai.archit@gmail.com>
2026-09-15 13:41:28 +0200

    Add the VALDO dataset for cerebral microbleed segmentation

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -128,4 +128,5 @@ from .totalsegmentator import get_totalsegmentator_dataset, get_totalsegmentator
 from .totalsegmentator_mri import get_totalsegmentator_mri_dataset, get_totalsegmentator_mri_loader
 from .upenn_gbm import get_upenn_gbm_dataset, get_upenn_gbm_loader
 from .uwaterloo_skin import get_uwaterloo_skin_dataset, get_uwaterloo_skin_loader
+from .valdo import get_valdo_dataset, get_valdo_loader
 from .verse import get_verse_dataset, get_verse_loader
