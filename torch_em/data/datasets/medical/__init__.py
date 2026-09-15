f93256aa0a5ee2c8d47c50e7300aa3ddb1bd94e7
anwai98 <anwai.archit@gmail.com>
2026-09-15 13:41:18 +0200

    Add the ATM dataset for airway tree segmentation

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -11,6 +11,7 @@ from .amos import get_amos_dataset, get_amos_loader
 from .aortaseg24 import get_aortaseg24_dataset, get_aortaseg24_loader
 from .atlas_liver import get_atlas_liver_dataset, get_atlas_liver_loader
 from .atlas_stroke import get_atlas_stroke_dataset, get_atlas_stroke_loader
+from .atm22 import get_atm22_dataset, get_atm22_loader
 from .autopet import get_autopet_dataset, get_autopet_loader
 from .btcv import get_btcv_dataset, get_btcv_loader
 from .busi import get_busi_dataset, get_busi_loader
