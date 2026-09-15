3c3e1aa66e2a4dc6874283b88743f7a27c3f366a
anwai98 <anwai.archit@gmail.com>
2026-09-15 13:41:17 +0200

    Add the ATLAS Stroke dataset for stroke lesion segmentation

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -10,6 +10,7 @@ from .amd_sd import get_amd_sd_dataset, get_amd_sd_loader
 from .amos import get_amos_dataset, get_amos_loader
 from .aortaseg24 import get_aortaseg24_dataset, get_aortaseg24_loader
 from .atlas_liver import get_atlas_liver_dataset, get_atlas_liver_loader
+from .atlas_stroke import get_atlas_stroke_dataset, get_atlas_stroke_loader
 from .autopet import get_autopet_dataset, get_autopet_loader
 from .btcv import get_btcv_dataset, get_btcv_loader
 from .busi import get_busi_dataset, get_busi_loader
