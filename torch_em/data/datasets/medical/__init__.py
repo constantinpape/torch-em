45a67bcf27e39e517de6d39b6c63953dc63d396c
anwai98 <anwai.archit@gmail.com>
2026-09-15 13:41:22 +0200

    Add the Mendeley MS dataset for multiple sclerosis lesion segmentation

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -73,6 +73,7 @@ from .mama_mia import get_mama_mia_dataset, get_mama_mia_loader
 from .mbh_seg import get_mbh_seg_dataset, get_mbh_seg_loader
 from .mcrib import get_mcrib_dataset, get_mcrib_loader
 from .mediastinal_ct import get_mediastinal_ct_dataset, get_mediastinal_ct_loader
+from .mendeley_ms import get_mendeley_ms_dataset, get_mendeley_ms_loader
 from .mice_tumseg import get_mice_tumseg_dataset, get_mice_tumseg_loader
 from .micro_usp import get_micro_usp_dataset, get_micro_usp_loader
 from .montgomery import get_montgomery_dataset, get_montgomery_loader
