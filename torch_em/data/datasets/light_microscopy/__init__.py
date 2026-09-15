c81777054dea1ae117b9fa59c8efbfebc26aec2e
anwai98 <anwai.archit@gmail.com>
2026-09-15 13:41:16 +0200

    Add the BBBC046 dataset (FiloData3D) dataset for synthetic 3D time-lapse

--- torch_em/data/datasets/light_microscopy/__init__.py
+++ torch_em/data/datasets/light_microscopy/__init__.py
@@ -10,6 +10,7 @@ from .apacs23 import get_apacs23_loader, get_apacs23_dataset
 from .arvidsson import get_arvidsson_loader, get_arvidsson_dataset
 from .bac_mother import get_bac_mother_loader, get_bac_mother_dataset
 from .balf import get_balf_loader, get_balf_dataset
+from .bbbc046 import get_bbbc046_loader, get_bbbc046_dataset
 from .bccd import get_bccd_loader, get_bccd_dataset
 from .bmgd import get_bmgd_loader, get_bmgd_dataset
 from .bitdepth_nucseg import get_bitdepth_nucseg_loader, get_bitdepth_nucseg_dataset
