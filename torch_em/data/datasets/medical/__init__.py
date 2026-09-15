47e4b4545adfbecfef1d423fbf3ca675ae3ec661
anwai98 <anwai.archit@gmail.com>
2026-09-15 13:41:18 +0200

    Add the BrainMetShare dataset for brain metastasis segmentation

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -14,6 +14,7 @@ from .atlas_stroke import get_atlas_stroke_dataset, get_atlas_stroke_loader
 from .atm22 import get_atm22_dataset, get_atm22_loader
 from .atriaseg import get_atriaseg_dataset, get_atriaseg_loader
 from .autopet import get_autopet_dataset, get_autopet_loader
+from .brainmetshare import get_brainmetshare_dataset, get_brainmetshare_loader
 from .btcv import get_btcv_dataset, get_btcv_loader
 from .busi import get_busi_dataset, get_busi_loader
 from .camus import get_camus_dataset, get_camus_loader
