9b9e63f966bc1b2ec544603e0fb28c4fb87e439f
anwai98 <anwai.archit@gmail.com>
2026-09-15 19:36:53 +0200

    Add the FUMPE dataset for pulmonary embolism segmentation
    Converts the 35 CT angiography series and their MATLAB masks into hdf5. The slices are ordered by their
    instance number, which is the order the masks were created in, because two of the series are acquired
    from head to feet and sorting along the slice normal would flip their masks.

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -45,6 +45,7 @@ from .duke_liver import get_duke_liver_dataset, get_duke_liver_loader
 from .emidec import get_emidec_dataset, get_emidec_loader
 from .feta24 import get_feta24_dataset, get_feta24_loader
 from .flare import get_flare_dataset, get_flare_loader
+from .fumpe import get_fumpe_dataset, get_fumpe_loader
 from .fundus_avseg import get_fundus_avseg_dataset, get_fundus_avseg_loader
 from .han_seg import get_han_seg_dataset, get_han_seg_loader
 from .hcc_tace import get_hcc_tace_dataset, get_hcc_tace_loader
