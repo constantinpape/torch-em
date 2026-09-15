11326b3954a90ec2c40d64dfbcddf250485ef5bd
anwai98 <anwai.archit@gmail.com>
2026-09-15 13:41:21 +0200

    Add the InsectAnatomy dataset for brain segmentation

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -52,6 +52,7 @@ from .hil_toothseg import get_hil_toothseg_dataset, get_hil_toothseg_loader
 from .hntsmrg import get_hntsmrg_dataset, get_hntsmrg_loader
 from .hvsmr import get_hvsmr_dataset, get_hvsmr_loader
 from .idrid import get_idrid_dataset, get_idrid_loader
+from .insect_anatomy import get_insect_anatomy_dataset, get_insect_anatomy_loader
 from .ircadb import get_ircadb_dataset, get_ircadb_loader
 from .isic import get_isic_dataset, get_isic_loader
 from .isles import get_isles_dataset, get_isles_loader
