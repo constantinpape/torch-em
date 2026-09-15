b2028ae4778caadd69d728ddf8e78bbbc34b883c
anwai98 <anwai.archit@gmail.com>
2026-09-15 13:41:20 +0200

    Add the EMIDEC dataset for left ventricle and for myocardial infarction

---
+++ scripts/datasets/medical/check_emidec.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+from torch_em.data import MinForegroundSampler
+from torch_em.util.debug import check_loader
+from torch_em.data.datasets.medical.emidec import get_emidec_loader
+
+
+sys.path.append("..")
+
+
+def check_emidec():
+    from util import ROOT
+
+    loader = get_emidec_loader(
+        path=os.path.join(ROOT, "emidec"),
+        patch_shape=(1, 512, 512),
+        batch_size=1,
+        ndim=2,
+        resize_inputs=True,
+        download=True,
+        sampler=MinForegroundSampler(min_fraction=0.01),
+    )
+
+    check_loader(loader, 8, plt=True, save_path="./test.png")
+
+
+if __name__ == "__main__":
+    check_emidec()
