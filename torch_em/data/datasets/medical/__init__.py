eef767d809375944d0d71743036a80a6fa42878f
anwai98 <anwai.archit@gmail.com>
2026-09-17 00:37:50 +0200

    Add the PSMA-PET-CT-Lesions dataset for whole-body PET/CT lesion segmentation

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -136,6 +136,7 @@ from .prostate158 import get_prostate158_dataset, get_prostate158_loader
 from .prostate_edge_cases import get_prostate_edge_cases_dataset, get_prostate_edge_cases_loader
 from .prostatex import get_prostatex_dataset, get_prostatex_loader
 from .psfhs import get_psfhs_dataset, get_psfhs_loader
+from .psma_pet_ct import get_psma_pet_ct_dataset, get_psma_pet_ct_loader
 from .qin_lungct_seg import get_qin_lungct_seg_dataset, get_qin_lungct_seg_loader
 from .qin_prostate import get_qin_prostate_dataset, get_qin_prostate_loader
 from .ravir import get_ravir_dataset, get_ravir_loader
