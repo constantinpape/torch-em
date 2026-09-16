47462a8dcd7b11f1f7accee79f333de2b217489e
anwai98 <anwai.archit@gmail.com>
2026-09-16 22:38:06 +0200

    Add the COph100 dataset for infant fundus vessel segmentation

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -34,6 +34,7 @@ from .chaos import get_chaos_dataset, get_chaos_loader
 from .chest_xray_masks import get_chest_xray_masks_dataset, get_chest_xray_masks_loader
 from .cholecseg8k import get_cholecseg8k_dataset, get_cholecseg8k_loader
 from .colorectal_liver_mets import get_colorectal_liver_mets_dataset, get_colorectal_liver_mets_loader
+from .coph100 import get_coph100_dataset, get_coph100_loader
 from .couinaud import get_couinaud_dataset, get_couinaud_loader
 from .covid19_20 import get_covid19_20_dataset, get_covid19_20_loader
 from .covid19_seg import get_covid19_seg_dataset, get_covid19_seg_loader
