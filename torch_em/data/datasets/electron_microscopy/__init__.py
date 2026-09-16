fc7a06ed52f1eaaf71fb3c0231a6f12eabf97b04
Anwai Archit <anwai.archit@gmail.com>
2026-09-16 19:45:29 +0200

    Add the L4Dense dataset for neuron instance segmentation in mouse barrel cortex (#818)

--- torch_em/data/datasets/electron_microscopy/__init__.py
+++ torch_em/data/datasets/electron_microscopy/__init__.py
@@ -25,6 +25,7 @@ from .human_organoids import get_human_organoids_loader, get_human_organoids_dat
 from .hydra_vulgaris import get_hydra_loader, get_hydra_dataset
 from .isbi2012 import get_isbi_loader, get_isbi_dataset
 from .kasthuri import get_kasthuri_loader, get_kasthuri_dataset
+from .l4_dense_reconstruction import get_l4_dense_reconstruction_loader, get_l4_dense_reconstruction_dataset
 from .parlakgul_liver import get_parlakgul_liver_loader, get_parlakgul_liver_dataset
 from .synapseweb_hippocampus import get_synapseweb_hippocampus_loader, get_synapseweb_hippocampus_dataset
 from .lucchi import get_lucchi_loader, get_lucchi_dataset
