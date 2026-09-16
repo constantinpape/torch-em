6e6bd358229de84d14b4c91230ef749022e65162
anwai98 <anwai.archit@gmail.com>
2026-09-16 22:18:43 +0200

    Add the Spinal-Multiple-Myeloma-SEG dataset for spinal lesion instance segmentation

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -137,6 +137,7 @@ from .skm_tea import get_skm_tea_dataset, get_skm_tea_loader
 from .sliver07 import get_sliver07_dataset, get_sliver07_loader
 from .soft_tissue_sarcoma import get_soft_tissue_sarcoma_dataset, get_soft_tissue_sarcoma_loader
 from .spider import get_spider_dataset, get_spider_loader
+from .spinal_mm import get_spinal_mm_dataset, get_spinal_mm_loader
 from .spine_mets import get_spine_mets_dataset, get_spine_mets_loader
 from .structseg import get_structseg_dataset, get_structseg_loader
 from .toothfairy import get_toothfairy_dataset, get_toothfairy_loader
