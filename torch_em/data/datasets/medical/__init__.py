21b6522db9c0b2cbf175ebba480b491f1ef413ea
anwai98 <anwai.archit@gmail.com>
2026-09-17 14:54:12 +0200

    Add the ImageCAS dataset for coronary artery segmentation in CT angiography

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -80,6 +80,7 @@ from .hva_ct import get_hva_ct_dataset, get_hva_ct_loader
 from .hvsmr import get_hvsmr_dataset, get_hvsmr_loader
 from .ibd_mre import get_ibd_mre_dataset, get_ibd_mre_loader
 from .idrid import get_idrid_dataset, get_idrid_loader
+from .imagecas import get_imagecas_dataset, get_imagecas_loader
 from .insect_anatomy import get_insect_anatomy_dataset, get_insect_anatomy_loader
 from .instance22 import get_instance22_dataset, get_instance22_loader
 from .intraretinal_cystoid_fluid import get_intraretinal_cystoid_fluid_dataset, get_intraretinal_cystoid_fluid_loader
