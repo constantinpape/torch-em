0d086bb0f3d28c91056d2b0c7e6006607749da20
anwai98 <anwai.archit@gmail.com>
2026-09-17 14:54:13 +0200

    Add the BraTS 2024 dataset for post-treatment glioma segmentation in brain MRI

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -23,6 +23,7 @@ from .bonbid_hie import get_bonbid_hie_dataset, get_bonbid_hie_loader
 from .brainmetshare import get_brainmetshare_dataset, get_brainmetshare_loader
 from .brainptm import get_brainptm_dataset, get_brainptm_loader
 from .brats import get_brats_dataset, get_brats_loader
+from .brats24 import get_brats24_dataset, get_brats24_loader
 from .btcv import get_btcv_dataset, get_btcv_loader
 from .btxrd import get_btxrd_dataset, get_btxrd_loader
 from .bus_uclm import get_bus_uclm_dataset, get_bus_uclm_loader
