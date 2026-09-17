349dfea95155ac46341dfd1de029bd7179402844
anwai98 <anwai.archit@gmail.com>
2026-09-17 11:43:09 +0200

    Add the ACRIN-6698 dataset for breast tumor functional-volume segmentation in DCE-MRI

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -4,6 +4,7 @@ from .abdomenct_1k import get_abdomenct_1k_dataset, get_abdomenct_1k_loader
 from .abus import get_abus_dataset, get_abus_loader
 from .acdc import get_acdc_dataset, get_acdc_loader
 from .acouslic_ai import get_acouslic_ai_dataset, get_acouslic_ai_loader
+from .acrin_6698 import get_acrin_6698_dataset, get_acrin_6698_loader
 from .acrin_hnscc import get_acrin_hnscc_dataset, get_acrin_hnscc_loader
 from .adrenal_acc import get_adrenal_acc_dataset, get_adrenal_acc_loader
 from .advanced_mri_breast_lesions import get_advanced_mri_breast_lesions_dataset, get_advanced_mri_breast_lesions_loader  # noqa
