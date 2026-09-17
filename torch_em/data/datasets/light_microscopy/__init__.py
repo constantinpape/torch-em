6bef8418da61bdeeb2765e78612fa309e3c8012e
anwai98 <anwai.archit@gmail.com>
2026-09-17 09:35:58 +0200

    Add the BBBC010 dataset for C. elegans instance segmentation

--- torch_em/data/datasets/light_microscopy/__init__.py
+++ torch_em/data/datasets/light_microscopy/__init__.py
@@ -3,6 +3,7 @@ from .alfi import get_alfi_loader, get_alfi_dataset
 from .bbbc024 import get_bbbc024_loader, get_bbbc024_dataset
 from .bbbc027 import get_bbbc027_loader, get_bbbc027_dataset
 from .bbbc030 import get_bbbc030_loader, get_bbbc030_dataset
+from .bbbc010 import get_bbbc010_loader, get_bbbc010_dataset
 from .bbbc032 import get_bbbc032_loader, get_bbbc032_dataset
 from .bbbc033 import get_bbbc033_loader, get_bbbc033_dataset
 from .bbbc034 import get_bbbc034_loader, get_bbbc034_dataset
