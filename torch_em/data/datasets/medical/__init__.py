d7fd378a6a39be57df3a263db81f4706d13016f1
anwai98 <anwai.archit@gmail.com>
2026-09-18 13:42:43 +0200

    Add the Mindboggle-101 dataset for cortical parcellation in brain MRI

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -128,6 +128,7 @@ from .mediastinal_ct import get_mediastinal_ct_dataset, get_mediastinal_ct_loade
 from .mendeley_ms import get_mendeley_ms_dataset, get_mendeley_ms_loader
 from .mice_tumseg import get_mice_tumseg_dataset, get_mice_tumseg_loader
 from .micro_usp import get_micro_usp_dataset, get_micro_usp_loader
+from .mindboggle101 import get_mindboggle101_dataset, get_mindboggle101_loader
 from .mnms import get_mnms_dataset, get_mnms_loader
 from .montgomery import get_montgomery_dataset, get_montgomery_loader
 from .mosmed_plus import get_mosmed_plus_dataset, get_mosmed_plus_loader
