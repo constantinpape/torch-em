584ef98c6ffd49da69305f395f3453ec9b5fe64d
anwai98 <anwai.archit@gmail.com>
2026-09-18 15:23:07 +0200

    Add the train split to the IUGC 2024 dataset loader

--- scripts/datasets/medical/check_iugc2024.py
+++ scripts/datasets/medical/check_iugc2024.py
@@ -7,17 +7,18 @@ from torch_em.data.datasets.medical.iugc2024 import get_iugc2024_loader
 DATA_ROOT = "/mnt/vast-nhr/projects/cidas/cca/data"
 
 
-def check_iugc2024():
+def check_iugc2024(split):
     loader = get_iugc2024_loader(
         path=os.path.join(DATA_ROOT, "iugc2024"),
         patch_shape=(512, 512),
         batch_size=2,
-        split="val",
+        split=split,
         download=True,
     )
 
-    check_loader(loader, 8, plt=True, save_path="./test.png")
+    check_loader(loader, 8, plt=True, save_path=f"./test_{split}.png")
 
 
 if __name__ == "__main__":
-    check_iugc2024()
+    for split in ["train", "val", "test"]:
+        check_iugc2024(split)
