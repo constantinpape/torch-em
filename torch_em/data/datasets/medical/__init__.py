05ea33de2e7f1626c2a37b0085f55d2ffdbba953
anwai98 <anwai.archit@gmail.com>
2026-09-20 19:54:23 +0200

    Add the C-TRUS dataset for colon wall segmentation in transabdominal ultrasound

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -60,6 +60,7 @@ from .ct_cadaiver import get_ct_cadaiver_dataset, get_ct_cadaiver_loader
 from .ct_lymph_nodes import get_ct_lymph_nodes_dataset, get_ct_lymph_nodes_loader
 from .ct_org import get_ct_org_dataset, get_ct_org_loader
 from .ctpelvic1k import get_ctpelvic1k_dataset, get_ctpelvic1k_loader
+from .ctrus import get_ctrus_dataset, get_ctrus_loader
 from .ctspine1k import get_ctspine1k_dataset, get_ctspine1k_loader
 from .ctspinopelvic1k import get_ctspinopelvic1k_dataset, get_ctspinopelvic1k_loader
 from .curvas import get_curvas_dataset, get_curvas_loader
