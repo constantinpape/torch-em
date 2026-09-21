f503d575766924ddd566f2a4314fb25ab9a81308
anwai98 <anwai.archit@gmail.com>
2026-09-21 18:27:46 +0200

    Add the RECIST-CT (HCUCH) dataset for multi-instance tumor, metastasis and lymph node segmentation in CT

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -227,6 +227,7 @@ from .qin_lungct_seg import get_qin_lungct_seg_dataset, get_qin_lungct_seg_loade
 from .qin_prostate import get_qin_prostate_dataset, get_qin_prostate_loader
 from .radgenome_chestct import get_radgenome_chestct_dataset, get_radgenome_chestct_loader
 from .ravir import get_ravir_dataset, get_ravir_loader
+from .recist_ct import get_recist_ct_dataset, get_recist_ct_loader
 from .refuge import get_refuge_dataset, get_refuge_loader
 from .remind import get_remind_dataset, get_remind_loader
 from .resect import get_resect_dataset, get_resect_loader
