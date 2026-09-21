e05d03751158e73c25e63774035748606e3274d4
anwai98 <anwai.archit@gmail.com>
2026-09-21 10:03:41 +0200

    Add the PU2756 dataset for pulmonary tumor segmentation in lung ultrasound

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -202,6 +202,7 @@ from .prostate_edge_cases import get_prostate_edge_cases_dataset, get_prostate_e
 from .prostatex import get_prostatex_dataset, get_prostatex_loader
 from .psfhs import get_psfhs_dataset, get_psfhs_loader
 from .psma_pet_ct import get_psma_pet_ct_dataset, get_psma_pet_ct_loader
+from .pu2756 import get_pu2756_dataset, get_pu2756_loader
 from .qata_cov19 import get_qata_cov19_dataset, get_qata_cov19_loader
 from .qin_lungct_seg import get_qin_lungct_seg_dataset, get_qin_lungct_seg_loader
 from .qin_prostate import get_qin_prostate_dataset, get_qin_prostate_loader
