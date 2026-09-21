6c247670b87efec947b3000970752fe752926e2b
anwai98 <anwai.archit@gmail.com>
2026-09-21 20:11:02 +0200

    Add the RCC-AID dataset for kidney, tumor and cyst segmentation in renal cell carcinoma CT

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -230,6 +230,7 @@ from .qin_lungct_seg import get_qin_lungct_seg_dataset, get_qin_lungct_seg_loade
 from .qin_prostate import get_qin_prostate_dataset, get_qin_prostate_loader
 from .radgenome_chestct import get_radgenome_chestct_dataset, get_radgenome_chestct_loader
 from .ravir import get_ravir_dataset, get_ravir_loader
+from .rcc_aid import get_rcc_aid_dataset, get_rcc_aid_loader
 from .recist_ct import get_recist_ct_dataset, get_recist_ct_loader
 from .refuge import get_refuge_dataset, get_refuge_loader
 from .remind import get_remind_dataset, get_remind_loader
