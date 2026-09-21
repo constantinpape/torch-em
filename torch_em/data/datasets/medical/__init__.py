0a7e4c4449aa86f21aee050aad6211256c3990f2
anwai98 <anwai.archit@gmail.com>
2026-09-21 15:00:55 +0200

    Add the ROSSA dataset for OCTA vessel segmentation
    Defaults to the 300 manually-annotated images; a SAM-assisted subset
    of 618 additional images is available via annotation="sam_assisted".

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -222,6 +222,7 @@ from .ribseg import get_ribseg_dataset, get_ribseg_loader
 from .rider_lung import get_rider_lung_dataset, get_rider_lung_loader
 from .riga_plus import get_riga_plus_dataset, get_riga_plus_loader
 from .rim_one_dl import get_rim_one_dl_dataset, get_rim_one_dl_loader
+from .rossa import get_rossa_dataset, get_rossa_loader
 from .rumc_kidney import get_rumc_kidney_dataset, get_rumc_kidney_loader
 from .sa_med2d import get_sa_med2d_dataset, get_sa_med2d_loader
 from .saros import get_saros_dataset, get_saros_loader
