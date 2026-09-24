31b6ff1cfc3e9ad2f6d0d52aa38f2e9f64321112
Anwai Archit <anwai.archit@gmail.com>
2026-09-24 14:22:46 +0200

    Add the C3RO dataset for crowdsourced multi-rater radiotherapy structure segmentation in CT

--- torch_em/data/datasets/medical/__init__.py
+++ torch_em/data/datasets/medical/__init__.py
@@ -44,6 +44,7 @@ from .btxrd import get_btxrd_dataset, get_btxrd_loader
 from .bus_uclm import get_bus_uclm_dataset, get_bus_uclm_loader
 from .busbra import get_busbra_dataset, get_busbra_loader
 from .busi import get_busi_dataset, get_busi_loader
+from .c3ro import get_c3ro_dataset, get_c3ro_loader
 from .cadis import get_cadis_dataset, get_cadis_loader
 from .camus import get_camus_dataset, get_camus_loader
 from .cap_lv import get_cap_lv_dataset, get_cap_lv_loader
